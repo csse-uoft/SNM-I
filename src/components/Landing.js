b50877281c3371d88b01f7bffcb9a2fb88291e57
tzuchiangweng <lisa.weng@mail.utoronto.ca>
2018-05-08 15:33:54 -0400

    Add landing page and remove previous existing pages

---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import _ from 'lodash'
+
+class Landing extends Component {
+  render() {
+    return(
+      <div className='landing'>
+      </div>
+    )
+  }
+}
+
+export default Landing;
