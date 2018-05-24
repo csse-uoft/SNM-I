63e3c4df248cd133308e62f90e72995b4a72f30e
tzuchiangweng <lisa.weng@mail.utoronto.ca>
2018-05-24 14:54:31 -0400

    Add userActions and userReducers

---
+++ src/store/actions/userActions.js
@@ -0,0 +1,152 @@
+import fetch from 'isomorphic-fetch';
+import { serverHost } from '../defaults.js';
+
+export const RECEIVE_NEW_USER = 'RECEIVE_NEW_USER';
+export const REQUEST_USER = 'REQUEST_USER';
+export const RECEIVE_USER = 'RECEIVE_USER';
+export const REQUEST_USERS = 'REQUEST_USERS';
+export const RECEIVE_USERS = 'RECEIVE_USERS';
+export const REMOVE_USER = 'REMOVE_USER';
+export const UPDATE_USER = 'UPDATE_USER';
+
+function receiveNewUser(json) {
+  return {
+    type: RECEIVE_NEW_USER,
+    user: json
+  }
+}
+
+function updatedUser(json) {
+  return {
+    type: UPDATE_USER,
+    user: json
+  }
+}
+
+function requestUser(id) {
+  return {
+    type: REQUEST_USER,
+    id: id
+  }
+}
+
+function receiveUser(id, json) {
+  return {
+    type: RECEIVE_USER,
+    id: id,
+    user: json,
+    receivedAt: Date.now()
+  }
+}
+
+function requestUsers() {
+  return {
+    type: REQUEST_USERS
+  }
+}
+
+function receiveUsers(json) {
+  return {
+    type: RECEIVE_USERS,
+    users: json,
+    receivedAt: Date.now()
+  }
+}
+
+function removeUser(id) {
+  return {
+    type: REMOVE_USER,
+    id: id
+  }
+}
+
+export function createUser(params) {
+  return dispatch => {
+    const url = serverHost + '/users/';
+    return fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(params),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `JWT ${localStorage.getItem('jwt_token')}`
+      }
+    }).then(response => response.json())
+      .then(json => dispatch(receiveNewUser(json)));
+  }
+}
+
+export function createUsers(params) {
+  return dispatch => {
+    const url = serverHost + '/users/';
+    return fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({csv: params})
+    })
+  }
+}
+
+
+export function updateUser(id, params) {
+  return dispatch => {
+    const url = serverHost + '/user/' + id + '/';
+    return fetch(url, {
+      method: 'PUT',
+      body: JSON.stringify(params),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `JWT ${localStorage.getItem('jwt_token')}`
+      }
+    }).then(response => response.json())
+      .then(json => dispatch(updatedUser(json)));
+  }
+}
+
+export function fetchUser(id) {
+  return dispatch => {
+    dispatch(requestUser(id))
+    const url = serverHost + '/user/' + id + '/';
+
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+          'Authorization': `JWT ${localStorage.getItem('jwt_token')}`
+        },
+      }).then(response => response.json())
+      .then(json => {
+        dispatch(receiveUser(id, json))
+      })
+  }
+}
+
+export function fetchUsers() {
+  return dispatch => {
+    dispatch(requestUsers())
+    const url = serverHost + '/users/';
+
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+          'Authorization': `JWT ${localStorage.getItem('jwt_token')}`
+        },
+      }).then(response => response.json())
+      .then(json => {
+        dispatch(receiveUsers(json))
+      })
+  }
+}
+
+export function deleteUser(id) {
+  return dispatch => {
+    const url = serverHost + '/user/' + id + '/';
+    return fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `JWT ${localStorage.getItem('jwt_token')}`
+      },
+    }).then(response => {
+      if (response.status === 204) {
+        dispatch(removeUser(id))
+      }
+    });
+  }
+}
