aa5895d6118e7bc3facb3149137e96524edb3230
tzuchiangweng <lisa.weng@mail.utoronto.ca>
2018-05-30 16:10:06 -0400

    Put routes into another file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
 
 // components
-import Landing from './components/Landing';
-import Login from './components/Login';
-import LoginPane from './components/LoginPane';
-import Dashboard from './components/Dashboard';
-import Clients from './components/Clients';
-import Client from './components/clients/Client';
-import ClientForm from './components/clients/ClientForm'
-import AnonymousClientForm from './components/clients/AnonymousClientForm'
-import Users from './components/Users';
-import User from './components/users/User';
-import UserForm from './components/users/UserForm'
-import NeedForm from './components/client_needs/NeedForm'
 import TopNavbar from './components/layouts/TopNavbar'
-import Providers from './components/Providers.js';
-import ProviderTypePrompt from './components/providers/ProviderTypePrompt.js'
-import AddServicePrompt from './components/providers/AddServicePrompt.js'
-import IndividualProviderForm from './components/providers/IndividualProviderForm.js'
-import OrganizationProviderForm from './components/providers/OrganizationProviderForm.js'
-import ProviderProfile from './components/providers/ProviderProfile.js'
-import EditProvider from './components/providers/EditProviderProfile.js'
-import Services from './components/Services';
-import Service from './components/services/Service';
-import ServiceForm from './components/services/ServiceForm'
 import Footer from './components/layouts/Footer'
+import routes from "./routes";
 
 // style
 import './stylesheets/App.css';
@@ -35,34 +13,7 @@ class App extends Component {
     return (
       <div className="App">
         <TopNavbar />
-        <Route exact path='/' component={Landing} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/login-pane' component={LoginPane} />
-        <Route exact path='/dashboard' component={Dashboard} />
-        <Route exact path='/clients' component={Clients} />
-        <Route exact path='/client/:id' component={Client} />
-        <Route exact path='/service/:id' component={Service} />
-        <Route exact path='/clients/new' component={ClientForm} />
-        <Route exact path='/services/new' component={ServiceForm} />
-        <Route exact path='/clients/anonymous/new' component={AnonymousClientForm} />
-        <Route exact path='/clients/:id/edit' component={ClientForm} />
-        <Route exact path='/users' component={Users} />
-        <Route exact path='/user/:id' component={User} />
-        <Route exact path='/users/new' component={UserForm} />
-        <Route exact path='/users/:id/edit' component={UserForm} />
-        <Route exact path='/clients/:id/needs/new' component={NeedForm} />
-        <Route exact path='/needs/:need_id/edit' component={NeedForm} />
-        <Route exact path='/services' component={Services} />
-        <Route exact path='/services/:id/edit' component={ServiceForm} />
-
-        <Route exact path='/providers' component={ Providers} />
-        <Route exact path='/providers/new' component={ProviderTypePrompt} />
-        <Route exact path='/providers/new/add-service' component={AddServicePrompt} />
-        <Route exact path='/providers/new/individual' component={IndividualProviderForm} />
-        <Route exact path='/providers/new/organization' component={OrganizationProviderForm} />
-        <Route exact path='/provider/:id' component={ProviderProfile} />
-        <Route exact path='/provider/:id/edit/individual' component={EditProvider} />
-        <Route exact path='/provider/:id/edit/organization' component={EditProvider} />
+        {routes}
         <Footer />
       </div>
     );
---
+++ src/routes.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Switch } from 'react-router';
+import { Route } from 'react-router-dom';
+
+// components
+import Landing from './components/Landing';
+import Login from './components/Login';
+import LoginPane from './components/LoginPane';
+import Dashboard from './components/Dashboard';
+import Clients from './components/Clients';
+import Client from './components/clients/Client';
+import ClientForm from './components/clients/ClientForm'
+import AnonymousClientForm from './components/clients/AnonymousClientForm'
+import Users from './components/Users';
+import User from './components/users/User';
+import UserForm from './components/users/UserForm'
+import NeedForm from './components/client_needs/NeedForm'
+import PrivateRoute from './components/routes/PrivateRoute'
+import AdminRoute from './components/routes/AdminRoute'
+import Providers from './components/Providers';
+import ProviderTypePrompt from './components/providers/ProviderTypePrompt'
+import AddServicePrompt from './components/providers/AddServicePrompt'
+import IndividualProviderForm from './components/providers/IndividualProviderForm'
+import OrganizationProviderForm from './components/providers/OrganizationProviderForm'
+import ProviderProfile from './components/providers/ProviderProfile'
+import EditProvider from './components/providers/EditProviderProfile'
+import Services from './components/Services';
+import Service from './components/services/Service';
+import ServiceForm from './components/services/ServiceForm'
+
+const routes = (
+  <Switch>
+    <Route exact path='/' component={Landing} />
+    <Route path='/login' component={Login} />
+    <Route path='/login-pane' component={LoginPane} />
+    <PrivateRoute path='/dashboard' component={Dashboard} />
+
+    <PrivateRoute path='/clients/:id/edit' component={ClientForm} />
+    <PrivateRoute path='/clients/:id/needs/new' component={NeedForm} />
+    <PrivateRoute path='/client/:id' component={Client} />
+    <PrivateRoute path='/clients/new' component={ClientForm} />
+    <PrivateRoute path='/clients/anonymous/new' component={AnonymousClientForm} />
+    <PrivateRoute path='/clients' component={Clients} />
+
+    <AdminRoute path='/users/:id/edit' component={UserForm} />
+    <AdminRoute path='/user/:id' component={User} />
+    <AdminRoute path='/users/new' component={UserForm} />
+    <AdminRoute path='/users' component={Users} />
+
+    <PrivateRoute path='/needs/:need_id/edit' component={NeedForm} />
+
+    <PrivateRoute path='/providers/new/add-service' component={AddServicePrompt} />
+    <PrivateRoute path='/providers/new/individual' component={IndividualProviderForm} />
+    <PrivateRoute path='/providers/new/organization' component={OrganizationProviderForm} />
+    <PrivateRoute path='/providers/new' component={ProviderTypePrompt} />
+    <PrivateRoute path='/provider/:id/edit/individual' component={EditProvider} />
+    <PrivateRoute path='/provider/:id/edit/organization' component={EditProvider} />
+    <PrivateRoute path='/provider/:id' component={ProviderProfile} />
+    <PrivateRoute path='/providers' component={Providers} />
+
+    <PrivateRoute path='/services/:id/edit' component={ServiceForm} />
+    <PrivateRoute path='/service/:id' component={Service} />
+    <PrivateRoute path='/services/new' component={ServiceForm} />
+    <PrivateRoute path='/services' component={Services} />
+  </Switch>
+)
+
+export default routes;
