5454564d6cb0aff52cb742921685776fe52b5930
betty <betty-wang@live.ca>
2018-05-30 12:44:38 -0400

    removed functions from actions.js and commented out code from reducers.js

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -11,8 +11,6 @@ import { clients } from './reducers/clientReducer.js';
 import { providers } from './reducers/providerReducers.js';
 import _ from 'lodash';
 
-//import { RECEIVE_PROVIDER, REQUEST_PROVIDER, SEARCH_PROVIDERS } from './actions/providerActions.js'
-
 function searchResultsByNeedId(state = {}, action) {
   let nextResultObj;
   switch (action.type) {
@@ -64,42 +62,6 @@ function goods(state = {index: [], loaded: false}, action) {
       return state
   }
 }
-
-
-
-// function providers(state = {index: [], byId: {}, loaded: false, value: '', filteredProviders: []}, action) {
-//   let nextIndex, nextById, providers, prevIndex;
-//   switch (action.type) {
-//     case REQUEST_PROVIDERS:
-//       return {...state, loaded: false};
-//     case RECEIVE_PROVIDERS:
-//       return {index: action.providers, filteredProviders: action.providers, loaded: true};
-//     case RECEIVE_NEW_PROVIDER:
-//       nextIndex = [action.provider, ...state.index]
-//       return {...state, index: nextIndex}
-//     case REMOVE_PROVIDER:
-//       nextIndex = _.clone(state.index);
-//       _.remove(nextIndex, (n) => { return n.id === action.id });
-//       return {...state, index: nextIndex}
-//     case RECEIVE_PROVIDER: 
-//       nextById = {...state.byId, [action.id]: { ...action.provider, loaded: true }}
-//       return {...state, byId: nextById }
-//     case REQUEST_PROVIDER:
-//       nextById = { ...state.byId, [action.id]: { loaded: false } }
-//       return {...state, byId: nextById }
-//     case SEARCH_PROVIDERS:
-//       if (action.value === '') {
-//         return {index: [...state.index], filteredProviders: [...state.index], loaded: true}
-//       }
-//       else {
-//         providers = [...state.index].filter((provider) => (provider.first_name).includes(action.value));
-//         return {index: [...state.index], filteredProviders: providers, loaded: true}
-//       }
-//     default:
-//       return state
-//   }
-// }
-
 
 export const rootReducer = combineReducers({
   searchResultsByNeedId,
