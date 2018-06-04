d20f17fdacc2007fabce3369457b2ae1db8fe730
tzuchiangweng <lisa.weng@mail.utoronto.ca>
2018-06-04 17:06:59 -0400

    Remove unused anonymous client form

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -29,11 +29,6 @@ class Clients extends Component {
               Add new named client profile
             </Button>
           </Link>
-          {/* <Link to={`/clients/anonymous/new`}>
-            <Button bsStyle="default">
-              Add new anonymous client profile
-            </Button>
-          </Link> */}
           <hr/>
           { p.clientsLoaded &&
             <ClientsIndex>{
