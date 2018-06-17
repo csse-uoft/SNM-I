df6ad02892398a1786299be22c48bb30f215b8ba
sh65 <38886655+sh65@users.noreply.github.com>
2018-06-17 13:58:49 -0400

    Add files via upload

--- src/components/Goods.js
+++ src/components/Goods.js
@@ -4,16 +4,47 @@ import _ from 'lodash';
 
 import GoodsIndex from './goods/GoodsIndex.js'
 import GoodRow from './goods/GoodRow.js'
+import CSVUploadModal from './shared/CSVUploadModal'
 
 // redux
 import { connect } from 'react-redux'
-import { fetchGoods, searchGoods } from '../store/actions/goodActions.js'
+import { fetchGoods, searchGoods, createGoods, GOOD_ERROR } from '../store/actions/goodActions.js'
 
 // styles
 import { Button } from 'react-bootstrap';
 import '../stylesheets/Client.css';
 
 class Goods extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleHide = this.handleHide.bind(this);
+    this.handleShow = this.handleShow.bind(this);
+    this.handleSubmit = this.handleSubmit .bind(this);
+
+    this.state = {
+      show: false
+    };
+  }
+
+  handleHide() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true })
+  }
+
+  handleSubmit(e) {
+    const file = document.querySelector('input[type="file"]').files[0];
+    this.props.dispatch(createGoods(file)).then((status) => {
+      if (status === GOOD_ERROR) {
+      } else {
+        this.setState({ show: false })
+      }
+    });
+  }
+
   componentWillMount() {
     this.props.dispatch(fetchGoods());
   }
@@ -21,7 +52,7 @@ class Goods extends Component {
   render() {
     const p = this.props;
     return(
-      <div className='clients-table content'>
+      <div className='clients-table content modal-container'>
         <div>
           <h1>Goods</h1>
           <Link to='/goods/new'>
@@ -29,6 +60,9 @@ class Goods extends Component {
               Add new good
             </Button>
           </Link>
+          <Button bsStyle="default"  onClick={this.handleShow}>
+            Add goods by uploading CSV
+          </Button>
           <hr/>
           { p.goodsLoaded &&
             <GoodsIndex>{
@@ -38,6 +72,11 @@ class Goods extends Component {
             }</GoodsIndex>
           }
         </div>
+        <CSVUploadModal
+          show={this.state.show}
+          onHide={this.handleHide}
+          submit={this.handleSubmit}
+        />
       </div>
     )
   }
