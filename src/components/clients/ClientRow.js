3628a5af8fc50fdbe163d4e0d50f297015c62ab6
tzuchiangweng <lisa.weng@mail.utoronto.ca>
2018-06-28 16:49:59 -0400

    Display DeleteModal when user clicks on the trashcan glyphicon and ask user to fill in the reason

--- src/components/clients/ClientRow.js
+++ src/components/clients/ClientRow.js
@@ -4,23 +4,12 @@ import { Glyphicon, Button } from 'react-bootstrap';
 
 // redux
 import { connect } from 'react-redux'
-import { deleteClient } from '../../store/actions/clientActions.js'
 
 class ClientRow extends Component {
-  constructor(props) {
-    super(props);
-
-    this.delete = this.delete.bind(this);
-  }
-
-  delete(id) {
-    this.props.dispatch(deleteClient(id));
-  }
-
   render() {
     const client = this.props.client;
 
-    return(
+    return (
       <tr>
         <td>{client.id}</td>
         <td>
@@ -39,7 +28,7 @@ class ClientRow extends Component {
           </Link>
         </td>
         <td>
-          <Button bsStyle="danger" onClick={() => this.delete(client.id)}>
+          <Button bsStyle="danger" onClick={() => this.props.handleShow(client.id)}>
             <Glyphicon glyph="trash" />
           </Button>
         </td>
