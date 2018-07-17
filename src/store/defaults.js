d91f82a9b2b70e54243ca00f5c84f18d166348d3
tzuchiangweng <lisa.weng@mail.utoronto.ca>
2018-07-17 13:57:19 -0400

    serverHost depends on the hostname of the react application

--- src/store/defaults.js
+++ src/store/defaults.js
@@ -1,1 +1,11 @@
-export const serverHost = 'http://127.0.0.1:8000';
+const hostname = window && window.location && window.location.hostname;
+let backendHost;
+
+if (hostname === 'social-needs-marketplace.herokuapp.com') {
+  backendHost = 'https://snm-impact-server.herokuapp.com';
+}
+else {
+  backendHost = 'http://127.0.0.1:8000';
+}
+
+export const serverHost = backendHost;
