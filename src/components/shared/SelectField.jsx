c3cf7db61bc6d1cd477498081471d591cdb44b19
tzuchiangweng <lisa.weng@mail.utoronto.ca>
2018-07-29 21:00:25 -0400

    Refactor SelectField to take both object and array options

--- src/components/shared/SelectField.jsx
+++ src/components/shared/SelectField.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import { Col, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 
@@ -14,8 +15,14 @@ export default function SelectField({ id, label, required, options, defaultOptio
       <Col sm={9}>
         <FormControl {...props}>
           <option value="select">--- {defaultOptionTitle || 'Not Set'} ---</option>
-          {options.map(option =>
-            <option key={option} value={option}>{option}</option>
+          { (options.constructor === Array) ? (
+            _.map(options, option =>
+              <option key={option} value={option}>{option}</option>
+            )
+          ) : (
+            _.map(options, (label, value) =>
+              <option key={value} value={value}>{label}</option>
+            )
           )}
         </FormControl>
       </Col>
