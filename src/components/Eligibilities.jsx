a19c931f3ab6eb863605479b01c9270c0cbb6516
tzuchiangweng <lisa.weng@mail.utoronto.ca>
2018-08-02 16:36:28 -0400

    Update eligibility criteria table

--- src/components/Eligibilities.jsx
+++ src/components/Eligibilities.jsx
@@ -1,21 +1,59 @@
+import _ from 'lodash';
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import _ from 'lodash';
+import { ACTION_ERROR } from '../store/defaults.js'
 
 import EligibilitiesIndex from './eligibilities/EligibilitiesIndex'
 import EligibilityRow from './eligibilities/EligibilityRow'
+import DeleteModal from './shared/DeleteModal'
 
 // redux
 import { connect } from 'react-redux'
-import { fetchEligibilities } from '../store/actions/eligibilityActions.js'
+import { fetchEligibilities, deleteEligibility } from '../store/actions/eligibilityActions.js'
 
 // styles
 import { Button } from 'react-bootstrap';
 import '../stylesheets/Client.css';
 
 class Eligibilities extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleDeleteModalHide = this.handleDeleteModalHide.bind(this);
+    this.handleDeleteModalShow = this.handleDeleteModalShow.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+
+    this.state = {
+      deleteModalShow: false,
+      objectId: null,
+    };
+  }
+
   componentWillMount() {
     this.props.dispatch(fetchEligibilities());
+  }
+
+  handleDeleteModalHide() {
+    this.setState({ deleteModalShow: false });
+  }
+
+  handleDeleteModalShow(id) {
+    this.setState({
+      deleteModalShow: true,
+      objectId: id
+    })
+  }
+
+  handleDelete(id, form) {
+    this.props.dispatch(
+      deleteEligibility(id, form, status => {
+        if (status === ACTION_ERROR) {
+          // this.setState({ displayError: true });
+        } else {
+          this.setState({ deleteModalShow: false });
+        }
+      })
+    );
   }
 
   render() {
@@ -26,17 +64,30 @@ class Eligibilities extends Component {
           <h1>Eligibility Criteria</h1>
           <Link to={`/eligibility_criteria/new`}>
             <Button bsStyle="default">
-              Create new eligibility Criteria
+              Create new eligibility criteria
             </Button>
           </Link>
           <hr/>
-          { p.eligibilitiesLoaded &&
+          {p.eligibilitiesLoaded &&
             <EligibilitiesIndex>{
               _.map(p.eligibilitiesById, (eligibility) => {
-                return <EligibilityRow key={eligibility.id} eligibility={eligibility} />
+                return (
+                  <EligibilityRow
+                    key={eligibility.id}
+                    eligibility={eligibility}
+                    handleShow={this.handleDeleteModalShow}
+                  />
+                );
               })
             }</EligibilitiesIndex>
           }
+          <DeleteModal
+            contentType='Eligibility Criteria'
+            objectId={this.state.objectId}
+            show={this.state.deleteModalShow}
+            onHide={this.handleDeleteModalHide}
+            delete={this.handleDelete}
+          />
         </div>
       </div>
     )
