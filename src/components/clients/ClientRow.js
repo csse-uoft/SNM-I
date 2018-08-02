883451282e3de690c0766b56ad9dd0f662db0c6f
tzuchiangweng <lisa.weng@mail.utoronto.ca>
2018-08-02 16:26:38 -0400

    Make DropdownMenu a component and refactor ClientRow

--- src/components/clients/ClientRow.js
+++ src/components/clients/ClientRow.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Glyphicon, Dropdown, MenuItem } from 'react-bootstrap';
 import { formatLocation } from '../../helpers/location_helpers'
 import { formatPhoneNumber } from '../../helpers/phone_number_helpers'
+import DropdownMenu from '../shared/DropdownMenu'
+
 
 class ClientRow extends Component {
   render() {
@@ -14,37 +15,16 @@ class ClientRow extends Component {
         <td>
           {formatPhoneNumber(client.primary_phone_number)}
         </td>
-        <td>
-          {client.email}
-        </td>
+        <td>{client.email}</td>
         <td>
           {formatLocation(client.address)}
         </td>
         <td>
-          <Dropdown
-            id="dropdown-menu"
-            className="vertical-options"
-            pullRight
-          >
-          <Dropdown.Toggle noCaret>
-            <Glyphicon glyph="option-vertical" />
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <MenuItem eventKey="1" href={`/clients/${client.id}`}>
-              View
-              <Glyphicon glyph="file" />
-            </MenuItem>
-            <MenuItem eventKey="2" href={`/clients/${client.id}/edit`}>
-              Edit
-              <Glyphicon glyph="pencil" />
-            </MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey="3" onClick={() => this.props.handleShow(client.id)}>
-              Delete
-              <Glyphicon glyph="trash" />
-            </MenuItem>
-          </Dropdown.Menu>
-        </Dropdown>
+          <DropdownMenu
+            urlPrefix="clients"
+            objectId={client.id}
+            handleShow={this.props.handleShow}
+          />
         </td>
       </tr>
     )
