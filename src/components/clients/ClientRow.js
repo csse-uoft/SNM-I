557e402291e26198c014c72b290f9b9b4d655a15
Shivam Bhatt <shivam.bhatt@mail.utoronto.ca>
2018-08-09 17:22:28 -0400

    A

--- src/components/clients/ClientRow.js
+++ src/components/clients/ClientRow.js
@@ -17,6 +17,7 @@ class ClientRow extends Component {
         </td>
         <td>{client.email}</td>
         <td>
+          {formatLocation(client.address)}
         </td>
         <td>
           <DropdownMenu
