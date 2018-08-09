885282d16204859ea96827b8ad047a181358a900
sh65 <38886655+sh65@users.noreply.github.com>
2018-08-08 20:05:42 -0400

    Add files via upload

--- src/components/services/ServiceSearchBar.js
+++ src/components/services/ServiceSearchBar.js
@@ -11,24 +11,33 @@ class ServiceSearchBar extends Component {
   constructor(props) {
     super(props);
     this.handleInput=this.handleInput.bind(this);
-    this.handleTypeChange=this.handleTypeChange.bind(this);
+    this.handleSearchChange=this.handleSearchChange.bind(this);
+    this.handleSortChange=this.handleSortChange.bind(this);
     this.state = {
       searchText: '',
-      searchType: 'name'
+      searchType: 'name',
+      sortType: 'name'
     }
   }
   
   handleInput(event) {
     const value = event.target.value;
     this.setState({ searchText: value});
-    this.props.dispatch(searchServices(value, this.state.searchType));
+    this.props.dispatch(searchServices(value, this.state.searchType, this.state.sortType));
   }
 
-  handleTypeChange(event) {
+  handleSearchChange(event) {
     const value = event.target.value;
     console.log(value);
     this.setState({searchType: value});
-    this.props.dispatch(searchServices(this.state.searchText, value));
+    this.props.dispatch(searchServices(this.state.searchText, value, this.state.sortType));
+  }
+
+  handleSortChange(event) {
+    const value = event.target.value;
+    console.log(value);
+    this.setState({sortType: value});
+    this.props.dispatch(searchServices(this.state.searchText, this.state.searchType, value));
   }
 
   render() {
@@ -44,7 +53,16 @@ class ServiceSearchBar extends Component {
         </FormGroup> {' '}
         <FormGroup controlId="searchBy">
           <ControlLabel> Search by: </ControlLabel>{' '}
-          <FormControl componentClass="select" placeholder="select" onChange={this.handleTypeChange}>
+          <FormControl componentClass="select" placeholder="select" onChange={this.handleSearchChange}>
+            <option value="name"> Name </option>
+            <option value="provider"> Provider </option>
+            <option value="description"> Description </option>
+            <option value="category"> Category </option>
+          </FormControl>
+        </FormGroup>{' '}
+        <FormGroup controlId="sortBy">
+          <ControlLabel> Sort by: </ControlLabel>{' '}
+          <FormControl componentClass="select" placeholder="select" onChange={this.handleSortChange}>
             <option value="name"> Name </option>
             <option value="provider"> Provider </option>
             <option value="description"> Description </option>
