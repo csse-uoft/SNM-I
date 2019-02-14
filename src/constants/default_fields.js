5bde8e7ba28ceb1ab511e6ffad71677754294af7
tzuchiangweng <lisa.weng@mail.utoronto.ca>
2019-02-14 12:14:14 -0500

    Add constant profileFields

---
+++ src/constants/default_fields.js
@@ -0,0 +1,11 @@
+export const profileFields = {
+  first_name: '',
+  middle_name: '',
+  last_name: '',
+  preferred_name: '',
+  gender: '',
+  birth_date: '',
+  email: '',
+  primary_phone_number: '',
+  alt_phone_number: '',
+}
