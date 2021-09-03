01a8e354119f33ed6cecb2477dcf9568a0f2469e
LesterLyu <lvds2000@gmail.com>
2021-09-03 11:11:43 -0400

    Move footer style to footer component. Remove underline when not hovered.

--- frontend/src/components/layouts/Footer.js
+++ frontend/src/components/layouts/Footer.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { Typography, Link } from "@material-ui/core";
+import { Typography, Link, Box } from "@material-ui/core";
 
 export default function () {
   return (
-    <div className="footer">
+    <Box sx={{
+      position: 'fixed',
+      left: '0',
+      bottom: '0',
+      width: '100%',
+      backgroundColor: '#222',
+      color: '#aaa',
+      textAlign: 'center',
+      borderRadius: '2px 2px 0 0',
+      alignContent: 'center',
+      padding: '4px',
+      '@media print': {
+        display: 'none'
+      }
+    }}>
       <Typography variant="caption">
-        <Link href="http://csse.utoronto.ca/" target="_blank" style={{color: "#ababab"}}>
+        <Link href="https://csse.utoronto.ca/" target="_blank" sx={{color: "#ababab", textDecoration: 'none'}}>
           Centre for Social Services Engineering
         </Link>, University of Toronto
       </Typography>
-    </div>
+    </Box>
   );
 };
