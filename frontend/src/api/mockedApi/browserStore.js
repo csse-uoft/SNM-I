7de63ed8ea710209550a524c676355368cbce0c2
LesterLyu <lvds2000@gmail.com>
2021-09-08 23:42:27 -0400

    Implement BrowserCounterStore and fix bug in BrowserStore.

--- frontend/src/api/mockedApi/browserStore.js
+++ frontend/src/api/mockedApi/browserStore.js
@@ -1,7 +1,7 @@
 export class BrowserStore {
   constructor(name, defaultValue) {
     this.name = name;
-    const rawData = localStorage.getItem(name);
+    const rawData = localStorage.getItem(this.name);
     this.store = rawData && JSON.parse(rawData) || defaultValue;
   }
 
@@ -13,6 +13,23 @@ export class BrowserStore {
   set value(newValue) {
     console.log('SET ', this.name, newValue);
     this.store = newValue;
-    localStorage.setItem(name, JSON.stringify(newValue));
+    localStorage.setItem(this.name, JSON.stringify(newValue));
+  }
+
+  save() {
+    localStorage.setItem(this.name, JSON.stringify(this.store));
+  }
+}
+
+export class BrowserCounterStore {
+  constructor(name, startNumber = 0) {
+    this.name = name + '-cnt';
+    this.store = Number(localStorage.getItem(this.name)) || (startNumber - 1);
+  }
+
+  get nextCnt() {
+    this.store++;
+    localStorage.setItem(this.name, this.store);
+    return this.store;
   }
 }
