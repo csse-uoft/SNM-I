dd6e95a2938d06a7fffd08a67cfa8ff497e78029
LesterLyu <lvds2000@gmail.com>
2022-06-20 13:54:51 -0400

    [frontend] Increase container maxWidth.

--- frontend/src/components/shared/GenericPage.js
+++ frontend/src/components/shared/GenericPage.js
@@ -117,7 +117,7 @@ export default function GenericPage(props) {
 
   return (
     <Fade in>
-      <Container>
+      <Container maxWidth="xl">
         <DataTable
           columns={columns}
           data={state.data}
