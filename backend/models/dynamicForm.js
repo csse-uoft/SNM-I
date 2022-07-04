1b4fe3feb51fd8d4479727ae5dcc99d457775c98
LesterLyu <lvds2000@gmail.com>
2022-07-04 16:12:16 -0400

    Update dynamicForm.js

--- backend/models/dynamicForm.js
+++ backend/models/dynamicForm.js
@@ -4,8 +4,11 @@ const MDBDynamicFormModel = new mongoose.Model('DynamicForm', {
   // Form type could be 'client', 'service', 'provider', 'user', ...
   formType: {type: String, required: true},
 
-  // The organization that this form is for.
+  // The organization that this form is for. Org IRI
   forOrganization: {type: String},
+
+  // UserAccount IRI
+  createdBy: {type: String, required: true},
 
   // Plain JSON object
   formStructure: {type: Object, required: true},
