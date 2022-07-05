9dabe346ba0045aaac599dc472f58119a51b6349
Luke Ren <luke.ren@mail.utoronto.ca>
2022-07-05 18:07:11 -0400

    The frontend and the backend of the forgot password process: new email sending function to send correct token link to the user specifically for resetPassword

--- backend/services/email.js
+++ backend/services/email.js
@@ -1,16 +1,15 @@
-const {sendVerificationMail} = require("../utils");
+const {sendResetPasswordEmail} = require("../utils");
 const {sign} = require("jsonwebtoken");
 const {jwtConfig} = require("../config");
 
 
 const sendVerificationEmail = async (req, res, next) => {
-  console.log(req.body)
   const {email} = req.body
   try {
     const token = sign({
       email
     }, jwtConfig.secret, jwtConfig.options)
-    await sendVerificationMail(email, token)
+    await sendResetPasswordEmail(email, token)
     return res.status(200).json({success: true, message: 'Successfully sent link'})
   }catch (e){
     next(e)
