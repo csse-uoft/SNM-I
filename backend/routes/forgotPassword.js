71a4e40e83ad42e69dbf3638da12840fca437922
Luke Ren <luke.ren@mail.utoronto.ca>
2022-07-06 13:57:52 -0400

    implemented the backend of reset password for forgot password process

--- backend/routes/forgotPassword.js
+++ backend/routes/forgotPassword.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const { fetchSecurityQuestionsByEmail, checkSecurityQuestion} = require("../services/securityQuestions")
 const {sendVerificationEmail} = require("../services/email");
+const {verifyUserForgotPassword} = require("../services/verifyUser");
 const router = express.Router();
 
 router.put('/securityQuestions/email', fetchSecurityQuestionsByEmail)
 router.post('/checkSecurityQuestion', checkSecurityQuestion)
 router.post('/sendVerificationEmail', sendVerificationEmail)
-router.post('/resetPassword/verify', )
+router.post('/resetPassword/verify', verifyUserForgotPassword)
 
 module.exports = router;
