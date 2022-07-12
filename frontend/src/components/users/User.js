017b5bcd7c4dd5e3b50bd94b7b55984ee09ac7e3
Zhaoyi Cheng <zhaoyicheng2001@gmail.com>
2022-07-12 12:42:11 -0400

    frontend added for manage user profile.

--- frontend/src/components/users/User.js
+++ frontend/src/components/users/User.js
@@ -3,29 +3,22 @@ import TR from '../shared/TR'
 import {fetchUser, getProfile, getUserProfileById} from '../../api/userApi'
 import { useParams } from "react-router";
 import { Link, Loading } from "../shared";
-import { Container, Paper, Table, Typography, TableBody, Button } from "@mui/material";
-import {userProfileFields} from "../../constants/userProfileFields";
+import { Container, Paper, Table, Typography, TableBody } from "@mui/material";
 
 export default function User() {
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState({});
   const {id} = useParams();
-  const [form, setForm] = useState({});
-  const profileForm = {
-    givenName: '',
-    familyName: '',
-    telephone: null,
-    email: '',
-    altEmail: '',
-  }
+
   useEffect(() => {
     getUserProfileById(id).then(user => {
-      setForm(user);
+      setUser(user);
       setLoading(false);
     });
   }, [id]);
 
- if (loading)
+
+  if (loading)
    return <Loading message={`Loading user...`}/>;
 
   console.log(user)
@@ -34,38 +27,42 @@ export default function User() {
       <Typography variant="h5" gutterBottom>
         User profile
       </Typography>
-      <Link to={`/users/${id}/edit`}>
-        <Button color="primary" variant="contained" style={{marginBottom: 12}}>
-          Edit
-        </Button>
-      </Link>
+      {/*<Link to={`/users/${id}/edit`}>*/}
+      {/*  <Button color="primary" variant="contained" style={{marginBottom: 12}}>*/}
+      {/*    Edit*/}
+      {/*  </Button>*/}
+      {/*</Link>*/}
 
       <Paper elevation={4}>
         <Table>
           <TableBody>
-            <TR
-              title="First Name"
-              value={user.givenName}
-            />
-            <TR
-              title="Last Name"
-              value={user.familyName}
-            />
-            <TR
-              title="Username"
-              value={user.email}
-            />
-            <TR
-              title="altEmail"
-              value={user.altEmail}
-            />
+            {/*<TR*/}
+            {/*  title="Display Name"*/}
+            {/*  value={user.displayName}*/}
+            {/*/>*/}
             <TR
               title="Role"
-              value={user.isSuperuser ? 'Admin' : 'User'}
+              value={user.role}
+            />
+            <TR
+              title="Given Name"
+              value={!user.primaryContact ? 'Not Provided' : user.primaryContact.givenName}
+            />
+            <TR
+              title="Family Name"
+              value={!user.primaryContact ? 'Not Provided' : user.primaryContact.familyName}
+            />
+            <TR
+              title="Username/Primary Email"
+              value={user.primaryEmail}
+            />
+            <TR
+              title="Secondary Email"
+              value={!user.secondaryEmail ? 'Not Provided' : user.secondaryEmail}
             />
             <TR
               title="Phone Number"
-              value={user.primary_phone_number}
+              value={'Not Provided'}
             />
           </TableBody>
         </Table>
