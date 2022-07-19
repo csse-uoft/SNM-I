dc61ed46f559ae299d3922f85f16a3117addcf5e
Zhaoyi Cheng <zhaoyicheng2001@gmail.com>
2022-07-19 17:08:03 -0400

    implemented GDBCIModel

--- backend/models/characteristic.js
+++ backend/models/characteristic.js
@@ -1,11 +1,10 @@
 const {createGraphDBModel, Types} = require("../utils/graphdb");
-const {GDBCharacteristicImplementationModel} = require("./characteristicImplementation");
+const {GDBCIModel} = require("./characteristicImplementation");
 
 const GDBCharacteristicModel = createGraphDBModel({
-  //name: {type: String, internalKey: 'cp:hasName'},
   description: {type: String, internalKey: 'cids:hasDescription'},
   codes: {type: [Types.NamedIndividual], internalKey: 'cids:hasCode'},
-  characteristicImplementation : {type: GDBCharacteristicImplementationModel, internalKey: 'snmi:CharacteristicImplementation'},
+  characteristicImplementation : {type: GDBCIModel, internalKey: 'snmi:CharacteristicImplementation'},
 }, {
   rdfTypes: ['snmi:Characteristic'], name: 'characteristic'
 });
--- backend/models/characteristicImplementation.js
+++ backend/models/characteristicImplementation.js
@@ -1,13 +1,21 @@
 const {createGraphDBModel, Types} = require("../utils/graphdb");
 
-const GDBCharacteristicImplementationModel = createGraphDBModel({
-  // name: {type: String, internalKey: 'cp:hasName'},
-  // description: {type: String, internalKey: 'cids:hasDescription'},
-  // codes: {type: [Types.NamedIndividual], internalKey: 'cids:hasCode'},
+/**
+ * GDBCIModel stands for GDB - Characteristic Implementation - Model
+ * @type {GDBUtils.GraphDBModelConstructor}
+ */
+const GDBCIModel = createGraphDBModel({
+  label: {type: String, internalKey: ':hasLabel'},
+  multipleValues:{type: Boolean, internalKey: ':hasMultipleValues'},
+  valueDataType:{type: Types.NamedIndividual, internalKey: ':hasValueDataType'},
+  fieldType:{type: GDBFieldTypeModel, internalKey: ':hasFieldType'},
+  options:{type: [Types.NamedIndividual], internalKey: ':hasOption'},
+  required:{type: Boolean, internalKey: ':isRequired'},
+  optionsFromClass:{type: Types.NamedIndividual, internalKey: ''},
 }, {
   rdfTypes: ['snmi:CharacteristicImplementation'], name: 'characteristicImplementation'
 });
 
 module.exports = {
-  GDBCharacteristicImplementationModel
+  GDBCIModel
 }
