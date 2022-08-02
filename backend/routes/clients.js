d2cf8fc4c01f422aecf96065f40fea7a4226a3cb
Zhaoyi Cheng <zhaoyicheng2001@gmail.com>
2022-08-02 15:56:42 -0400

    update on question occ

--- backend/routes/clients.js
+++ backend/routes/clients.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const {createQuestionOcc, createClient} = require("../services/client");
+const {createClient} = require("../services/client");
 const router = express.Router();
 //TODO: implement backend functions from ../services/client
 
-router.post('/questionOcc', createQuestionOcc);
 router.post('/client', createClient);
 
 module.exports = router;
--- backend/services/client/questionOcc.js
+++ backend/services/client/questionOcc.js
@@ -1,7 +1,7 @@
 const {GDBQOModel} = require("../../models");
 const {findQuestionById} = require("../question/questionHelper");
 
-async function createQuestionOcc(id, answer) {
+async function createQuestionOccurrence(id, answer) {
   const question = await findQuestionById(id);
   const questionOcc = GDBQOModel({
     stringValue: answer,
@@ -12,19 +12,4 @@ async function createQuestionOcc(id, answer) {
   return questionOcc;
 }
 
-const createQuestionOcc = async (id, answer) => {
-  // assume we get id of the question and answer of that question in req.body
-
-  try {
-    if (!id){
-      return res.status(202).json({success: true, message: 'No questionOcc need to be created.'});
-    }
-    await createQuestionOccHelper(id, answer);
-    return res.status(202).json({success: true, message: 'Successfully create QuestionOcc.'});
-  } catch (e) {
-    next(e)
-  }
-
-}
-
-module.exports = {createQuestionOcc}
+module.exports = {createQuestionOccurrence}
