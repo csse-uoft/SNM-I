bdb459c8db016ec702307ec45975c1222d3993cd
Zhaoyi Cheng <zhaoyicheng2001@gmail.com>
2022-08-02 12:40:27 -0400

    updating type for dataValue in question Occurrence model

--- backend/models/ClientFunctionalities/questionOccurrence.js
+++ backend/models/ClientFunctionalities/questionOccurrence.js
@@ -1,8 +1,8 @@
-const {createGraphDBModel, DeleteType, Types} = require("../../utils/graphdb");
+const {createGraphDBModel, DeleteType} = require("../../utils/graphdb");
 const {GDBQuestionModel} = require("./question");
 
 const GDBQuestionOccurrenceModel = createGraphDBModel({
-  dataValue: {type: Types.NamedIndividual, internalKey: ':hasDataValue'},
+  dataValue: {type: String, internalKey: ':hasDataValue'},
   occurrence: {type: GDBQuestionModel, internalKey: ':occurrenceOf', onDelete: DeleteType.CASCADE},
 }, {
   rdfTypes: [':QuestionOccurrence'], name: 'questionOccurrence'
