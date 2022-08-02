98ba3f188a9f9fea01913ad086bf93ab4bc7a492
Zhaoyi Cheng <zhaoyicheng2001@gmail.com>
2022-08-02 12:46:26 -0400

    adding CO in client model

--- backend/models/ClientFunctionalities/client.js
+++ backend/models/ClientFunctionalities/client.js
@@ -3,8 +3,8 @@ const {GDBQOModel} = require("./questionOccurrence");
 
 
 const GDBClientModel = createGraphDBModel({
-  // characteristicOcc: {type: [GDBCharacteristicOccurrenceModel],
-  //   internalKey: ':hasCharacteristicOccurrence', onDelete: DeleteType.CASCADE},
+  characteristicOcc: {type: [GDBCOModel],
+    internalKey: ':hasCharacteristicOccurrence', onDelete: DeleteType.CASCADE},
   questionOcc: {type: [GDBQOModel],
     internalKey: ':hasQuestionOccurrence', onDelete: DeleteType.CASCADE},
 }, {
