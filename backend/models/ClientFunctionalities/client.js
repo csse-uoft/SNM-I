f51a591732e2c3a6627debc0a2b788be29402839
Zhaoyi Cheng <zhaoyicheng2001@gmail.com>
2022-08-02 12:32:07 -0400

    create client model

---
+++ backend/models/ClientFunctionalities/client.js
@@ -0,0 +1,16 @@
+const {createGraphDBModel, DeleteType} = require("../../utils/graphdb");
+const {GDBQuestionOccurrenceModel} = require("./questionOccurence");
+
+
+const GDBClientModel = createGraphDBModel({
+  // characteristicOcc: {type: [GDBCharacteristicOccurrenceModel],
+  //   internalKey: ':hasCharacteristicOccurrence', onDelete: DeleteType.CASCADE},
+  questionOcc: {type: [GDBQuestionOccurrenceModel],
+    internalKey: ':hasQuestionOccurrence', onDelete: DeleteType.CASCADE},
+}, {
+  rdfTypes: [':Client'], name: 'client'
+});
+
+module.exports = {
+  GDBClientModel
+}
