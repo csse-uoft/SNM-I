2a028424af6398d1dc5a740249558a6aecafba8c
Zhaoyi Cheng <zhaoyicheng2001@gmail.com>
2022-08-02 13:43:53 -0400

    minor changes in GDBCOModel

--- backend/models/ClientFunctionalities/CharacteristicOccurrence.js
+++ backend/models/ClientFunctionalities/CharacteristicOccurrence.js
@@ -1,16 +1,19 @@
 const {createGraphDBModel, Types, DeleteType} = require("../../utils/graphdb");
 const {GDBCharacteristicModel} = require("./characteristic");
 
-
+/**
+ * GDBCOModel stands for GDB - Characteristic Occurrence - Model
+ * @type {GDBUtils.GraphDBModelConstructor}
+ */
 const GDBCOModel = createGraphDBModel({
   dataStringValue: {type: String, internalKey: ':hasStringValue'},
   dataNumberValue: {type: Number, internalKey: ':hasNumberValue'},
   dataBooleanValue: {type: Boolean, internalKey: ':hasBooleanValue'},
   dataDateValue: {type: Date, internalKey: 'hasDateValue'},
   objectValue: {type: [Types.NamedIndividual], internalKey: ':hasObjectValue'},
-  occurrenceOf: {type: GDBCharacteristicModel, internalKey: ':occurrenceOf'},
+  occurrenceOf: {type: GDBCharacteristicModel, internalKey: ':occurrenceOf', onDelete: DeleteType.CASCADE},
 }, {
-  rdfTypes: [':CharacteristicImplementation'], name: 'characteristicImplementation'
+  rdfTypes: [':CharacteristicOccurrence'], name: 'characteristicOccurrence'
 });
 
 module.exports = {
