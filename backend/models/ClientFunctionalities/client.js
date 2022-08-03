f375222939786cc125c8f985b64af40a71e772b3
Luke Ren <luke.ren@mail.utoronto.ca>
2022-08-03 12:33:52 -0400

    update and create models

--- backend/models/ClientFunctionalities/client.js
+++ backend/models/ClientFunctionalities/client.js
@@ -1,13 +1,15 @@
 const {createGraphDBModel, DeleteType} = require("../../utils/graphdb");
 const {GDBQOModel} = require("./questionOccurrence");
-const {GDBCOModel} = require("./CharacteristicOccurrence");
+const {GDBCOModel} = require("./characteristicOccurrence");
+const {GDBNoteModel} = require("./note");
 
 
 const GDBClientModel = createGraphDBModel({
-  characteristicOcc: {type: [GDBCOModel],
+  characteristicOccurrence: {type: [GDBCOModel],
     internalKey: ':hasCharacteristicOccurrence', onDelete: DeleteType.CASCADE},
-  questionOcc: {type: [GDBQOModel],
+  questionOccurrence: {type: [GDBQOModel],
     internalKey: ':hasQuestionOccurrence', onDelete: DeleteType.CASCADE},
+  note: {type: [GDBNoteModel], internalKey: 'hasNote'}
 }, {
   rdfTypes: [':Client'], name: 'client'
 });
