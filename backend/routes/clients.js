23c5a7099fafb3322a799055737aac680a145283
Luke Ren <luke.ren@mail.utoronto.ca>
2022-08-03 15:32:08 -0400

    implemented createClient api

--- backend/routes/clients.js
+++ backend/routes/clients.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const {createClient} = require("../services/client");
 const router = express.Router();
-//TODO: implement backend functions from ../services/client
+
+router.post('/client', createClient);
 
 
 
--- backend/services/client/client.js
+++ backend/services/client/client.js
@@ -1,11 +1,71 @@
 const {findClientById, createClientHelper, updateClientHelper} = require("./clientHelper");
-const {updateQuestion} = require("../question/question");
 
-const createUpdateClient = async (req, res, next) => {
+const {MDBDynamicFormModel} = require("../../models/dynamicForm");
+const {GDBCharacteristicModel} = require("../../models");
+const {GDBQuestionModel} = require("../../models/ClientFunctionalities/question");
+const {GDBCOModel} = require("../../models/ClientFunctionalities/characteristicOccurrence");
+
+const fieldParser = (fieldName) => {
+  const arr = fieldName.split('_')
+  return arr
+}
+
+const createClient = async (req, res, next) => {
   const data = req.body;
-  if(!Array.isArray(data.questionOccurrences) || !Array.isArray(data.characteristicOccurrences)){
-    return res.status(400).json({success: false, message: 'Wrong format on request body'})
+  if(!data.formId){
+    return res.status(400).json({success: false, message: 'No form Id is given'})
   }
+  const form = await MDBDynamicFormModel.findById(data.formId)
+  if(form.formType !== 'client'){
+    return res.status(400).json({success: false, message: 'The form is not for client'})
+  }
+  if(!form.formStructure){
+    return res.status(400).json({success: false, message: 'The form structure is undefined'})
+  }
+  // TODO: verify if questions and characteristics are in the form
+
+  if(!data.fields){
+    return res.status(400).json({success: false, message: 'Please provide the fields'})
+  }
+  const characteristicIds = []
+  const characteristicValues = []
+  const questionIds = []
+  const questionValues = []
+  Object.entries(data.fields).map((field, value) => {
+    const [fieldType, fieldId] = fieldParser(field)
+    if(fieldType === 'characteristic'){
+      characteristicIds.push(fieldId)
+      characteristicValues.push(value)
+    }else if(fieldType === 'question'){
+      questionIds.push(fieldId)
+      questionValues.push(value)
+    }
+  })
+
+  const characteristics = await GDBCharacteristicModel.find({_id: {$in: characteristicIds}})
+  const questions = await GDBQuestionModel.find({_id: {$in: questionIds}})
+
+  characteristics.forEach((characteristic, index) => {
+    const newCO = GDBCOModel({occurrenceOf: characteristic})
+    if(characteristic.implementation.valueDataType == 'xsd: string'){
+      // TODO: check if the dataType of input value is correct
+      newCO.dataStringValue = characteristicValues[index]
+    }else if(characteristic.implementation.valueDataType == 'xsd: number'){
+      newCO.dataNumberValue = characteristicValues[index]
+    }else if(characteristic.implementation.valueDataType == 'xsd:boolean'){
+      newCO.dataBooleanValue = characteristicValues[index]
+    }else if(characteristic.implementation.valueDataType == 'xsd:datetimes'){
+      newCO.dataDateValue = characteristicValues[index]
+    }else if(characteristic.implementation.valueDataType == "owl:NamedIndividual"){
+      newCO.objectValues = [... characteristicValues[index]]
+    }
+    newCO.save()
+  })
+
+
+  // if(!Array.isArray(data.questionOccurrences) || !Array.isArray(data.characteristicOccurrences)){
+  //   return res.status(400).json({success: false, message: 'Wrong format on request body'})
+  // }
   try {
     if(!data.id){
       await createClientHelper(data);
@@ -21,4 +81,4 @@ const createUpdateClient = async (req, res, next) => {
 }
 
 
-module.exports = {createUpdateClient, }
+module.exports = {createClient, }
