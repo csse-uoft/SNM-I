e386e5a091bfb0543de6a935e0120ed774f2b6a0
Zhaoyi Cheng <zhaoyicheng2001@gmail.com>
2022-08-04 07:44:42 -0400

    frontend api for organization

---
+++ frontend/src/api/organization.js
@@ -0,0 +1,21 @@
+import { deleteJson, getJson, postJson, putJson } from "./index";
+
+export async function createOrganization(body) {
+  return postJson('/api/clientOrOrganization/organization/', body);
+}
+
+export async function updateOrganization(id, body) {
+  return putJson('/api/organization/' + id, body);
+}
+
+export async function fetchOrganization(id) {
+  return getJson('/api/clientOrOrganization/organization/' + id);
+}
+
+export async function fetchOrganizations() {
+  return getJson('/api/organization');
+}
+
+export async function deleteOrganization(id) {
+  return deleteJson('/api/organization/delete/' + id);
+}
