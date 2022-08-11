1aea31a3465a74931f4ed70e772e2a75ab2f7c3b
LesterLyu <lvds2000@gmail.com>
2022-08-11 14:14:33 -0400

    Update Characteristic model.

--- backend/models/ClientFunctionalities/characteristic.js
+++ backend/models/ClientFunctionalities/characteristic.js
@@ -5,6 +5,8 @@ const GDBCharacteristicModel = createGraphDBModel({
   description: {type: String, internalKey: 'cids:hasDescription'},
   name: {type: String, internalKey: ':hasName'},
   codes: {type: [Types.NamedIndividual], internalKey: 'cids:hasCode'},
+  predefinedProperty: {type: String, internalKey: 'snmi:hasPredefinedProperty'},
+  isPredefined: {type: Boolean, internalKey: 'snmi:isPredefined'},
   implementation: {type: GDBCIModel, internalKey: ':hasCharacteristicImplementation', onDelete: DeleteType.CASCADE},
 }, {
   rdfTypes: [':characteristics'], name: 'characteristic'
