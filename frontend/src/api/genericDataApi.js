61d53ae5a65cc83f191f074d8119ca7ae131a833
Luke Ren <luke.ren@mail.utoronto.ca>
2022-08-18 16:04:02 -0400

    add updateSingleGeneric functionality

--- frontend/src/api/genericDataApi.js
+++ frontend/src/api/genericDataApi.js
@@ -7,3 +7,7 @@ export async function fetchSingleGeneric(option, id) {
 export async function createSingleGeneric(option, body) {
   return postJson(`/api/generic/${option}`, body)
 }
+
+export async function updateSingleGeneric(option, id, body) {
+  return putJson(`/api/genetic/${option}/${id}`, body)
+}
