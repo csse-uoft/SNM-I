4837fb40cdcf2b694e198736eb03bdffaab89dab
Luke Ren <luke.ren@mail.utoronto.ca>
2022-08-30 10:58:47 -0400

    update advancedSearch

--- backend/services/advancedSearch/index.js
+++ backend/services/advancedSearch/index.js
@@ -7,60 +7,66 @@ const {GraphDB, regexBuilder} = require("../../utils/graphdb");
 const genericType2Model = {
   client: GDBClientModel,
   organization: GDBOrganizationModel
-}
+};
 
 const genericItemType2Model = {
   characteristic: GDBCharacteristicModel,
   question: GDBQuestionModel
 
-}
+};
 
 // ex. return all characteristics associated with client
-async function fetchForAdvancedSearch(req, res, next){
-  try{
+async function fetchForAdvancedSearch(req, res, next) {
+  try {
     // genericType: client, organization...
     // genericItemType: characteristic, question ...
-    const {genericType, genericItemType} = req.params
-    if(!genericType || !genericItemType)
-      res.status(400).json({success: false, message:"genericType or genericItemType is not given"})
-    const usage = await MDBUsageModel.findOne({option: genericItemType, genericType: genericType})
+    const {genericType, genericItemType} = req.params;
+    if (!genericType || !genericItemType)
+      res.status(400).json({success: false, message: "genericType or genericItemType is not given"});
+    const usage = await MDBUsageModel.findOne({option: genericItemType, genericType: genericType});
     let data;
-    if(usage){
+    if (usage) {
       data = await Promise.all(usage.optionKeys.map(async id => {
-        return (await genericItemType2Model[genericItemType].findOne({_id: id}, {populates: ['implementation']}))
-      }))
+        return (await genericItemType2Model[genericItemType].findOne({_id: id}, {populates: ['implementation']}));
+      }));
     }
-    res.status(200).json({success: true, data: data || [], message: usage?'':`There is no such ${genericItemType} associated with ${genericType}.`})
-  }catch (e){
-    next(e)
+    res.status(200).json({
+      success: true,
+      data: data || [],
+      message: usage ? '' : `There is no such ${genericItemType} associated with ${genericType}.`
+    });
+  } catch (e) {
+    next(e);
   }
 }
 
 
 async function advancedSearchGeneric(req, res, next) {
   const {genericType, genericItemType} = req.params; // genericType: ex. client; genericItemType: ex. characteristic
-  const searchConditions = req.body; // searchConditions: ex. {1: 'emolee', 2: 'Cheng'}
+  const {searchConditions, searchTypes} = req.body; // searchConditions: ex. {1: 'emolee', 2: 'Cheng'}
   try {
-    const conditions = []
-    const key = genericItemType + 'Occurrences'
-    for(let genericItemId in searchConditions){
-      const value = searchConditions[genericItemId]
-      if(typeof value === 'string'){
-        conditions.push(
-          {occurrenceOf: `:${genericItemType}_${genericItemId}`, dataStringValue: {$regex: regexBuilder(value, 'i')}}
-        );
-      }else if(typeof value.min === 'number' && typeof value.max === 'number'){
+    const conditions = [];
+    const key = genericItemType + 'Occurrences';
+    for (let genericItemId in searchConditions) {
+      const value = searchConditions[genericItemId];
+      if (typeof value.min === 'number' && typeof value.max === 'number') { // number range
         conditions.push(
           {occurrenceOf: `:${genericItemType}_${genericItemId}`, dataNumberValue: {$lt: value.max, $gt: value.min}}
         );
+      } else if (undefined) {
+
+      } else { // string object
+        conditions.push(
+          {occurrenceOf: `:${genericItemType}_${genericItemId}`, dataStringValue: {$regex: regexBuilder(value, 'i')}}
+        );
       }
     }
-    const data = await genericType2Model[genericType].find({[key]: {$and: conditions}})
+    const data = await genericType2Model[genericType].find({[key]: {$and: conditions}});
     return res.status(200).json({success: true, data});
   } catch (e) {
-    next(e)
+    next(e);
   }
 }
 
 
-module.exports = {fetchForAdvancedSearch, advancedSearchGeneric}
+module.exports = {fetchForAdvancedSearch, advancedSearchGeneric};
