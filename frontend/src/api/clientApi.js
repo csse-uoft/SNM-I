0533b3b1d4762aad1e913b00a7af09159169425d
Zhaoyi Cheng <zhaoyicheng2001@gmail.com>
2022-09-01 11:50:03 -0400

    delete some not used code

--- frontend/src/api/clientApi.js
+++ frontend/src/api/clientApi.js
@@ -3,7 +3,6 @@ import { deleteJson, getJson, postJson, putJson } from "./index";
 /**
  * This function fetches all clients
  * This is used now in frontend/src/Clients.js to fetch for clients.
- * Could be replaced into fetchGeneric in the future
  * @returns {Promise<*>}
  */
 export async function fetchClients() {
@@ -12,7 +11,6 @@ export async function fetchClients() {
 
 /**
  * This function delete single client.
- * Could be replaced into deleteSingleGeneric in the future
  * @param id
  * @returns {Promise<*>}
  */
