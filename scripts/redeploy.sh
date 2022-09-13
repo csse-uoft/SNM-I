f0079c43edad809c8481f6128a9eee0c55c7af9a
Dishu(Lester) Lyu <lvds2000@gmail.com>
2022-09-13 18:11:19 -0400

    Update redeploy.sh

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -3,8 +3,8 @@
 # This script is meant to run on the ec2 directly.
 # Invoked when someone pushes commits.
 
-sudo -u ubuntu bash -c 'git reset --hard'
-sudo -u ubuntu bash -c 'git pull'
+sudo git reset --hard
+sudo git pull
 cp -f ./scripts/docker-compose.yml ./
 cp -f /home/ubuntu/.env ./backend/
 
