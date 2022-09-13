a32093f808e440477b6b26093a7dc2b82e852ded
Dishu(Lester) Lyu <lvds2000@gmail.com>
2022-09-13 18:09:09 -0400

    Update redeploy.sh

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -4,7 +4,7 @@
 # Invoked when someone pushes commits.
 
 sudo -u ubuntu bash -c 'git reset --hard'
-sudo -u ubuntu bash -c 'git pull origin master'
+sudo -u ubuntu bash -c 'git pull'
 cp -f ./scripts/docker-compose.yml ./
 cp -f /home/ubuntu/.env ./backend/
 
