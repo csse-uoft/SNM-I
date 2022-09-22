3205047d764c88e733e078e47f83628061b9623c
Luke Ren <luke.ren@mail.utoronto.ca>
2022-09-22 09:59:39 -0400

    add models

--- backend/models/needOccurrence.js
+++ backend/models/needOccurrence.js
@@ -1,9 +1,16 @@
 const {createGraphDBModel} = require("../utils/graphdb");
 const {GDBUserAccountModel} = require("./userAccount");
 const {GDBNeedModel} = require("./need");
+const {GDBServiceModel} = require("./service");
 
 const GDBNeedOccurrenceModel = createGraphDBModel({
-  occurrenceOf: {type: GDBNeedModel, internalKey: ':occurrenceOf'}
+  occurrenceOf: {type: GDBNeedModel, internalKey: ':occurrenceOf'},
+  startDate: {type: Date, internalKey: ':hasStartDate'},
+  endDate: {type: Date, internalKey: ':hasEndDate'},
+  modifiedAt: {type: Date, internalKey: ':modifiedAt'},
+  modifiedBy:{type: Date, internalKey: 'modifiedBy'},
+  acuity:{type: String, internalKey: ':hasAcuity'},
+  metByService:{type: GDBServiceModel, internalKey: 'metByService'},
 }, {
   rdfTypes: [':NeedOccurrence'], name: 'needOccurrence'
 });
