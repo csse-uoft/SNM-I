6af8d41149b50fd6d0d5cb92f9e9a60143d20877
Luke Ren <luke.ren@mail.utoronto.ca>
2022-09-22 10:27:48 -0400

    add models

--- backend/models/serviceOccurrence.js
+++ backend/models/serviceOccurrence.js
@@ -1,9 +1,19 @@
-const {createGraphDBModel, DeleteType, Types} = require("../utils/graphdb");
 const {GDBAddressModel} = require('./address')
 const {GDBCOModel} = require("./ClientFunctionalities/characteristicOccurrence");
 const {GDBServiceProviderModel} = require("./serviceProvider");
+const {createGraphDBModel, Types, DeleteType} = require("../utils/graphdb");
+const {GDBNeedModel} = require("./need");
+const {GDBServiceModel} = require("./service");
 
-const GDBServiceModel = createGraphDBModel({
+const GDBServiceOccurrenceModel = createGraphDBModel({
+  occurrenceOf: {type: GDBServiceModel, internalKey: ':occurrenceOf'},
+  startDate: {type: Date, internalKey: ':hasStartDate'},
+  endDate: {type: Date, internalKey: ':hasEndDate'},
+  mode: {type: {}, internalKey: ':hasMode'}, // todo
+  hoursOfOperation: {type: Number, internalKey: ':hasHoursOfOperation'},
+  address: {type: GDBAddressModel, internalKey: ':hasLocation'},
+
+
   name: {type: String, internalKey: 'tove_org:hasName'},
   code: {type: [Types.NamedIndividual], internalKey: 'cids:hasCode'},
   characteristicOccurrence: {type: [GDBCOModel],
@@ -20,5 +30,5 @@ const GDBServiceModel = createGraphDBModel({
 });
 
 module.exports = {
-  GDBServiceModel
+  GDBServiceOccurrenceModel
 }
