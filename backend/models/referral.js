6dafd53c139cbe32057cef921b713af8629a94ad
Luke Ren <luke.ren@mail.utoronto.ca>
2022-09-29 21:40:09 -0400

    update referral.js

--- backend/models/referral.js
+++ backend/models/referral.js
@@ -10,18 +10,12 @@ const {GDBProgramModel} = require("./program");
 const GDBReferralModel = createGraphDBModel({
   client: {type: GDBClientModel, internalKey: ':hasClient'},
   date: {type: Date, internalKey: ':hasDate'},
-  modifiedAt: {type: Date, internalKey: 'modifiedAt'},
-  modifiedBy: {type: GDBUserAccountModel, internalKey: 'modifiedBy'},
   referringServiceProvider: {type: GDBServiceProviderModel, internalKey: ':hasReferringServiceProvider'},
   receivingServiceProvider: {type: GDBServiceProviderModel, internalKey: ':hasReceivingServiceProvider'},
   note: {type: String, internalKey: 'hasNote'},
   description: {type: String, internalKey: 'hasDescription'},
-  need: {type: GDBNeedModel, internalKey: ':hasNeed'},
   needOccurrence: {type: GDBNeedOccurrenceModel, internalKey: ':hasNeedOccurrence'},
   service: {type: GDBServiceModel, internalKey: ':forService'},
-  program: {type: GDBProgramModel, internalKey: ':forProgram'}
-  // questionOccurrence: {type: [GDBQOModel],
-  //   internalKey: ':hasQuestionOccurrence', onDelete: DeleteType.CASCADE},
 
 }, {
   rdfTypes: [':Referral'], name: 'referral'
