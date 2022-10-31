cd3a5455d71605f73df8aea6ca27dc8933452efc
Luke Ren <luke.ren@mail.utoronto.ca>
2022-10-31 16:11:30 -0400

    update so update

--- backend/services/genericData/serviceOccurrenceInternalTypeTreater.js
+++ backend/services/genericData/serviceOccurrenceInternalTypeTreater.js
@@ -32,9 +32,13 @@ const serviceOccurrenceInternalTypeFetchTreater = async (data) => {
   const result = {};
   const schema =  data.schema;
   for (const property in data) {
-    if (property === 'occurrenceOf' || property === 'address' || property === 'needSatisfiers' || property ==='needSatisfierOccurrences') {
+    if (property === 'occurrenceOf' || property === 'address') {
       const internalType = await GDBInternalTypeModel.findOne({predefinedProperty: schema[property].internalKey, formType: 'serviceOccurrence'});
       result[ 'internalType_'+ internalType._id] = SPARQL.getFullURI(data[property]);
+    }else if(property === 'needSatisfiers'){
+      const propertyRemovedS = property.slice(0, -1);
+      const internalType = await GDBInternalTypeModel.findOne({predefinedProperty: schema[propertyRemovedS].internalKey, formType: 'serviceOccurrence'});
+      result[ 'internalType_'+ internalType._id] = data[property].map(SPARQL.getFullURI);
     }
   }
   return result;
