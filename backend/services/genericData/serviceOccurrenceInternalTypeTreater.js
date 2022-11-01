32f5741a59ae1de4bdc8aad857bda3d981022a1c
Luke Ren <luke.ren@mail.utoronto.ca>
2022-11-01 14:15:10 -0400

    fix bugs

--- backend/services/genericData/serviceOccurrenceInternalTypeTreater.js
+++ backend/services/genericData/serviceOccurrenceInternalTypeTreater.js
@@ -45,7 +45,60 @@ const serviceOccurrenceInternalTypeFetchTreater = async (data) => {
 };
 
 const serviceOccurrenceInternalTypeUpdateTreater = async (internalType, value, result) => {
-  await serviceOccurrenceInternalTypeCreateTreater(internalType, result, value);
+  const property = linkedProperty('serviceOccurrence', internalType);
+  if (property === 'occurrenceOf') {
+    result[property] = value;
+  } else if (property === 'needSatisfier') {
+    let previousNeedSatisfiers = result.needSatisfiers;
+    let previousNeedSatisfierOccurrences = result.needSatisfierOccurrences;
+
+
+    for (let key in previousNeedSatisfierOccurrences){
+      previousNeedSatisfierOccurrences[key] = await GDBNeedSatisfierOccurrenceModel.findById(previousNeedSatisfierOccurrences[key].split('_')[1]);
+    }
+
+    // check previous needSatisfer which no long in the list and remove their needSatisfierOccurrence
+    previousNeedSatisfiers = previousNeedSatisfiers.map(
+      (previousNeedSatisfier) => {
+        if(!value.includes(SPARQL.getFullURI(previousNeedSatisfier))){
+          // remove corresponding needSatisfierOccurrence
+          const correspondingNeedSatisfierOccurrence = previousNeedSatisfierOccurrences.filter(
+            (needSatisfierOccurrence) => {
+              return needSatisfierOccurrence.occurrenceOf === previousNeedSatisfier;
+          })[0];
+          previousNeedSatisfierOccurrences = previousNeedSatisfierOccurrences.filter(nso => {
+            return nso._id !== correspondingNeedSatisfierOccurrence._id
+          })
+          GDBNeedSatisfierOccurrenceModel.findByIdAndDelete(correspondingNeedSatisfierOccurrence._id);
+          return false;
+        } else {
+          return previousNeedSatisfier;
+        }
+      }
+    );
+
+    previousNeedSatisfiers = previousNeedSatisfiers.filter(previousNeedSatisfier => {
+      return previousNeedSatisfier;
+    })
+
+    // take new needSatisfiers and add correspongding needSatisfier Occurrence in
+    for (let needSatisfierURI of value) {
+      if(!previousNeedSatisfiers.includes(SPARQL.getPrefixedURI(needSatisfierURI))){
+        const [_, nsid] = needSatisfierURI.split('_');
+        const needSatisfierOccurrenceData = {occurrenceOf: needSatisfierURI};
+        const needSatisfier = await GDBNeedSatisfierModel.findById(nsid);
+        needSatisfierOccurrenceData.address = needSatisfier.address;
+        needSatisfierOccurrenceData.startDate = needSatisfier.startDate;
+        needSatisfierOccurrenceData.endDate = needSatisfier.endDate;
+        needSatisfierOccurrenceData.description = needSatisfier.description;
+        const needSatisfierOccurrence = GDBNeedSatisfierOccurrenceModel(needSatisfierOccurrenceData);
+        await needSatisfierOccurrence.save();
+        previousNeedSatisfierOccurrences.push(needSatisfierOccurrence);
+      }
+    }
+    result.needSatisfiers = value;
+    result.needSatisfierOccurrences = previousNeedSatisfierOccurrences;
+  }
 }
 
 module.exports = {serviceOccurrenceInternalTypeCreateTreater, serviceOccurrenceInternalTypeFetchTreater, serviceOccurrenceInternalTypeUpdateTreater}
