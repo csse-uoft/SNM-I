7c2cb384389430ebc6fc5fff63eee42ada2f476b
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2023-08-02 19:13:05 -0400

    Corrected Client Name Shown in the Webpage

--- frontend/src/components/clientAssessment/ClientAssesment.js
+++ frontend/src/components/clientAssessment/ClientAssesment.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from "../shared"
 import { GenericPage } from "../shared";
+import { getInstancesInClass } from "../../api/dynamicFormApi";
 import { deleteSingleGeneric, fetchMultipleGeneric } from "../../api/genericDataApi";
 
 const TYPE = 'clientAssessment';
@@ -32,7 +33,26 @@ export default function ClientAssessment() {
   };
 
   const fetchData = async () => {
-    const clientAssessments = (await fetchMultipleGeneric('clientAssessment')).data;
+    // get all clients data
+    const clientAssessments = (await fetchMultipleGeneric('appointment')).data;
+    const clients = {};
+    await getInstancesInClass(':Client').then((res) => {
+      Object.keys(res).forEach((key) => {
+        const clientId = key.split('#')[1];
+        clients[clientId] = res[key];
+      }
+      );
+    });
+    const persons = {};
+    // get all persons data
+    await getInstancesInClass('cids:Person').then((res) => {
+      Object.keys(res).forEach((key) => {
+        const personId = key.split('#')[1];
+        persons[personId] = res[key];
+      });
+    });
+
+    // const clientAssessments = (await fetchMultipleGeneric('clientAssessment')).data;
     const data = [];
     for (const clientAssessment of clientAssessments) {
       const clientAssessmentData = {_id: clientAssessment._id};
@@ -49,6 +69,18 @@ export default function ClientAssessment() {
             clientAssessmentData.outcome = occ.objectValue;
           }
         }
+      if (clientAssessment.client){
+        // get corresponding client data
+        clientAssessmentData.client = clients[clientAssessment.client.slice(1)]
+      }
+      if (clientAssessment.person){
+        // get corresponding person data
+        clientAssessmentData.person = persons[clientAssessment.person.slice(1)]
+      }
+      if (clientAssessment.user){
+        // const userData = await fetchSingleGeneric('user', appointment.user);
+        // console.log(userData);
+      }
 
       data.push(clientAssessmentData);
     }
--- frontend/src/components/clientAssessment/ClientAssessmentForm.js
+++ frontend/src/components/clientAssessment/ClientAssessmentForm.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import GenericForm from "../shared/GenericForm";
+import { fetchInternalTypeByFormType } from "../../api/internalTypeApi";
+import { AppointmentClientField } from '../appointments/AppointmentClientField';
 
 export default function ClientAssessmentForm() {
+
+  const formType = 'clientAssessment';
+  const [internalTypes, setInternalTypes] = useState({});
+  useEffect(() => {
+    fetchInternalTypeByFormType(formType).then(({ internalTypes }) => {
+      const data = {}
+      for (const { implementation, name, _id } of internalTypes) {
+        data[name] = { implementation, _id }
+      }
+      setInternalTypes(data);
+    });
+  }, []);
+
+  const handleRenderField = ({ required, id, type, implementation, content, _id }, index, fields, handleChange, step) => {
+    if (implementation.optionsFromClass === ":Client") {
+      return <AppointmentClientField
+        handleChange={handleChange}
+        clientFieldId={internalTypes.clientForClientAssessment._id}
+        step={step}
+        fields={fields}
+      />
+    } else if (implementation.label === "Last Name") {
+      return "";
+    } else if (implementation.label === "First Name") {
+      return "";
+    }
+  }
+  
   return (
-    <GenericForm name={'clientAssessment'} mainPage={'/clientAssessment'}/>
+    <GenericForm name={formType} mainPage={'/appointments'} onRenderField={handleRenderField} />
   );
+
 };
