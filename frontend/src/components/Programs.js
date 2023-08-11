bab0d707db54ecb5e6e0f49541a4090638e4bd8f
Subhasis Chakraborti <75278768+htysc@users.noreply.github.com>
2023-08-11 08:19:24 -0400

    Map markers working for Programs and ProgramOccurrences

--- frontend/src/components/Programs.js
+++ frontend/src/components/Programs.js
@@ -4,6 +4,8 @@ import { GenericPage } from "./shared";
 import { deleteSingleGeneric, fetchMultipleGeneric, fetchSingleGeneric } from "../api/genericDataApi";
 import { getServiceProviderName, getServiceProviderType } from "./shared/ServiceProviderInformation";
 import { getServiceProviderNameCharacteristicIds, getPersonNameCharacteristicIds } from "./shared/CharacteristicIds";
+import { getAddressCharacteristicId } from "./shared/CharacteristicIds";
+import { formatLocation } from '../helpers/location_helpers'
 
 const TYPE = 'programs';
 
@@ -62,34 +64,40 @@ const columnsWithoutOptions = [
 
 export default function Programs() {
 
-  const nameFormatter = program => program.name;
+  const nameFormatter = (program) => {return program.name;};
 
   const generateMarkers = (data, pageNumber, rowsPerPage) => {
-    return [];
     // TODO: verify this works as expected
     const currPagePrograms = data.slice((pageNumber - 1) * rowsPerPage, pageNumber * rowsPerPage);
     return currPagePrograms.map(program => ({
-      position: {lat: Number(program.location.lat), lng: Number(program.location.lng)},
-      title: program.name,
-      link: `/${TYPE}/${program.id}`,
-      content: program.desc,
+      position: {lat: Number(program.address.lat), lng: Number(program.address.lng)},
+      title: nameFormatter(program),
+      link: `/${TYPE}/${program._id}`,
+      content: program.address && formatLocation(program.address),
     })).filter(program => program.position.lat && program.position.lng);
   };
 
   const fetchData = async () => {
-    const programs = (await fetchMultipleGeneric('program')).data;
+    const programs = (await fetchMultipleGeneric('program')).data; // TODO: Does not contain address info
     const serviceProviderCharacteristicIds = (await getServiceProviderNameCharacteristicIds());
     const personCharacteristicIds = (await getPersonNameCharacteristicIds());
+    const addressCharacteristicId = await getAddressCharacteristicId(); // TODO: inefficient!
     const data = [];
     for (const program of programs) {
-      const programData = {_id: program._id};
+      const programData = {_id: program._id, address: {}};
       if (program.characteristicOccurrences)
         for (const occ of program.characteristicOccurrences) {
           if (occ.occurrenceOf?.name === 'Program Name') {
             programData.name = occ.dataStringValue;
           } else if (occ.occurrenceOf?.name === 'Service Provider') {
             programData.provider = occ.objectValue;
-          }
+          } else if (occ.occurrenceOf?.name === 'Address') {
+            const obj = (await fetchSingleGeneric("program", program._id)).data; // TODO: inefficient!
+            programData.address = {
+              lat: obj['characteristic_' + addressCharacteristicId].lat,
+              lng: obj['characteristic_' + addressCharacteristicId].lng,
+            };
+           }
         }
       if (program.serviceProvider) {
         programData.serviceProvider = {
