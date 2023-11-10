10c4fef5785d5ef9daf9e77add42806753594ebe
Subhasis Chakraborti <subhasis.chakraborti@mail.utoronto.ca>
2023-11-09 19:18:16 -0500

    refreshOrganization working

--- backend/services/partnerNetwork/index.js
+++ backend/services/partnerNetwork/index.js
@@ -8,7 +8,12 @@ async function refreshOrganization(req, res, next) {
   try {
     const {id} = req.params;
 
-    const organizationGeneric = await fetchSingleGenericHelper('organization', id);
+    let organizationGeneric;
+    try {
+      organizationGeneric = await fetchSingleGenericHelper('organization', id);
+    } catch (e) {
+      return res.status(404).json({message: 'Partner organization not found'});
+    }
 
     const organization = {};
     for (let [key, data] of Object.entries(organizationGeneric)) {
@@ -34,7 +39,24 @@ async function refreshOrganization(req, res, next) {
 
     if (organization.partner) {
       if (organization.endpointUrl && organization.endpointPort && organization.apiKey) {
-        return res.status(200).json({data: {'success': true}, success: true});
+        const url = new URL(organization.endpointUrl);
+        url.port = organization.endpointPort;
+
+        const controller = new AbortController();
+        const timeout = setTimeout(() => controller.abort(), 5000);
+        const response = await fetch(url, {
+          signal: controller.signal,
+          method: 'GET',
+          headers: {
+            'X-API-KEY': organization.apiKey,
+          },
+        });
+        clearTimeout(timeout);
+
+        if (response.status >= 400 && response.status < 600) {
+          return res.status(response.status).json({message: 'Bad response from partner: ' + response.status});
+        }
+        return res.json(await response.json());
       } else {
         return res.status(400).json({message: 'The partner organization does not have a valid URL, port, and/or API key'});
       }
@@ -42,7 +64,7 @@ async function refreshOrganization(req, res, next) {
       return res.status(400).json({message: 'The entity to refresh is not a partner organization'});
     }
   } catch (e) {
-    next(e);
+    return res.status(400).json({message: e?.message});
   }
 }
 
