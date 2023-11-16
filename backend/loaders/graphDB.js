3fa46730e8f032c08da5bd356a74e54a29e27d49
BenCheng2 <bencheng0609@gmail.com>
2023-11-16 16:09:09 -0500

    feat(Feature): Load all lucene connectors on the setup

--- backend/loaders/graphDB.js
+++ backend/loaders/graphDB.js
@@ -4,6 +4,8 @@ const {graphdb, mongodb} = require('../config');
 const {sleep} = require('../utils');
 const {namespaces} = require('./namespaces');
 const {initGraphDB, MongoDBIdGenerator, importRepositorySnapshot} = require("graphdb-utils");
+const fs = require("fs");
+const {EOL} = require("os");
 
 let repository;
 
@@ -32,6 +34,25 @@ async function cleanup() {
   await sendQuery("DROP NAMED");
 }
 
+async function setupConnector(){
+  // This is a function that loads every lucene connector
+  // The file of connector_setup_queries contains the name of the query on odd line, and
+  // query itself on the even line (Consider index starts from 1 not 0)
+  const fileContent = fs.readFileSync('loaders/connector_setup_queries', 'utf8')
+  const queries = fileContent.split(EOL)
+  // read the query not the name of the query
+  for (let i = 1; i < queries.length; i+=2) {
+    await fetch(queries[i], {
+      method: 'POST',
+      headers: {
+        ContentType: 'application/json'
+      }
+    });
+  }
+  console.log("All connectors are loaded")
+}
+
+
 
 async function load() {
   // ID Generator for creating new instances
@@ -54,6 +75,8 @@ async function load() {
   await cleanup();
   await importRepositorySnapshot('https://github.com/csse-uoft/compass-ontology/releases/download/latest/compass-ontology+dependencies.brf');
 
+  // Load all the connectors for lucene
+  await setupConnector()
 }
 
 
