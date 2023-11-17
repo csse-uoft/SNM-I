ba05792d8630448346722dbd06df88dd2861af39
Subhasis Chakraborti <subhasis.chakraborti@mail.utoronto.ca>
2023-11-16 20:05:19 -0500

    OrganizationStatusField

--- frontend/src/components/providers/OrganizationStatusField.js
+++ frontend/src/components/providers/OrganizationStatusField.js
@@ -1,71 +1,55 @@
 import {useEffect, useState} from "react";
-import FieldGroup from '../shared/FieldGroup';
+import FieldGroup from "../shared/FieldGroup";
+import SelectField from "../shared/fields/SelectField";
 
-export function HomeAndPartnerOrganizationField({
+export function OrganizationStatusField({
                   fields,
-                  isHomeFieldId,
-                  isPartnerFieldId,
+                  statusFieldId,
                   endpointUrlFieldId,
                   endpointPortFieldId,
                   apiKeyFieldId,
                   handleChange
                 }) {
-  const isHomeFieldKey = `characteristic_${isHomeFieldId}`;
-  const isPartnerFieldKey = `characteristic_${isPartnerFieldId}`;
+  const statusFieldKey = `characteristic_${statusFieldId}`;
   const endpointUrlFieldKey = `characteristic_${endpointUrlFieldId}`;
   const endpointPortFieldKey = `characteristic_${endpointPortFieldId}`;
   const apiKeyFieldKey = `characteristic_${apiKeyFieldId}`;
 
-  const [isHome, setIsHome] = useState(fields[isHomeFieldKey] === undefined ? true : fields[isHomeFieldKey]);
-  const [isPartner, setIsPartner] = useState(fields[isPartnerFieldKey] || false);
+  const [status, setStatus] = useState(fields[statusFieldKey] === undefined ? "Home" : fields[statusFieldKey]);
 
-  const handleChangeIsPartner = key => (e) => {
+  const handleChangeStatus = key => (e) => {
     const value = e.target.value;
-    setIsPartner(value);
+    setStatus(value);
     handleChange(key)(e);
 
-    // if the new isPartner value is false, unset the three inner fields
-    if (!value) {
+    // if the new status value is not 'Partner', unset the three inner fields
+    if (value !== "Partner") {
       handleChange(endpointUrlFieldKey)({target: {value: ''}});
       handleChange(endpointPortFieldKey)({target: {value: ''}});
       handleChange(apiKeyFieldKey)({target: {value: ''}});
     }
   }
 
-  const handleChangeIsHome = key => (e) => {
-    const value = e.target.value;
-    setIsHome(value);
-    handleChange(key)(e);
-
-    // if the new isHome value is true, unset the inner fields
-    if (value) {
-      handleChangeIsPartner(isPartnerFieldKey)({target: {value: false}});
-    }
-  }
+  const options = {
+    "Home": "Home",
+    "Partner": "Partner",
+    "Other": "Other",
+  };
 
   return <>
-    <FieldGroup component={'BooleanRadioField'} key={isHomeFieldKey}
-                label="Home Organization?" required value={fields[isHomeFieldKey]}
-                onChange={handleChangeIsHome(isHomeFieldKey)} controlled/>
-    {!isHome ?
+    <SelectField key={statusFieldKey} label="Organization Status" required value={fields[statusFieldKey]}
+                 options={options} onChange={handleChangeStatus(statusFieldKey)}/>
+    {fields[statusFieldKey] === "Partner" ?
       <div>
-        <FieldGroup component={'BooleanRadioField'} key={isPartnerFieldKey}
-                  label="Partner Organization?" required value={fields[isPartnerFieldKey]}
-                  onChange={handleChangeIsPartner(isPartnerFieldKey)} controlled/>
-        {isPartner ?
-          <div>
-            <FieldGroup component={'TextField'} key={endpointUrlFieldKey}
-                        label="Endpoint URL" required value={fields[endpointUrlFieldKey]}
-                        onChange={handleChange(endpointUrlFieldKey)}/>
-            <FieldGroup component={'NumberField'} key={endpointPortFieldKey}
-                        label="Endpoint Port Number" required value={fields[endpointPortFieldKey]}
-                        onChange={handleChange(endpointPortFieldKey)}/>
-            <FieldGroup component={'TextField'} key={apiKeyFieldKey}
-                        label="API Key" required value={fields[apiKeyFieldKey]}
-                        onChange={handleChange(apiKeyFieldKey)}/>
-          </div>
-          : null
-        }
+        <FieldGroup component={'TextField'} key={endpointUrlFieldKey}
+                    label="Endpoint URL" required value={fields[endpointUrlFieldKey]}
+                    onChange={handleChange(endpointUrlFieldKey)}/>
+        <FieldGroup component={'NumberField'} key={endpointPortFieldKey}
+                    label="Endpoint Port Number" required value={fields[endpointPortFieldKey]}
+                    onChange={handleChange(endpointPortFieldKey)}/>
+        <FieldGroup component={'TextField'} key={apiKeyFieldKey}
+                    label="API Key" required value={fields[apiKeyFieldKey]}
+                    onChange={handleChange(apiKeyFieldKey)}/>
       </div>
       : null
     }
