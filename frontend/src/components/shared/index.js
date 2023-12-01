b85b10ab9d4f77c4e736a5c2acdc37d11dd765af
BenCheng2 <bencheng0609@gmail.com>
2023-12-01 14:47:53 -0500

    feat(Feature): Add Advanced Search Bar

--- frontend/src/components/shared/index.js
+++ frontend/src/components/shared/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
     Menu,
     MenuItem,
@@ -9,11 +9,11 @@ import {
     Button,
     Popover,
     Popper,
-    Box
+    Box, Container
 } from '@mui/material';
-import { makeStyles } from "@mui/styles";
-import { Link as DomLink, useNavigate } from 'react-router-dom';
-import { Add as AddIcon, CloudUpload as UploadIcon } from '@mui/icons-material';
+import {makeStyles} from "@mui/styles";
+import {Link as DomLink, useNavigate} from 'react-router-dom';
+import {Add as AddIcon, CloudUpload as UploadIcon} from '@mui/icons-material';
 import GoogleMap from './GoogleMap';
 import CSVUploadModal from './CSVUploadModal';
 import DeleteModal from './DeleteModal';
@@ -24,36 +24,58 @@ import FieldsWrapper from "./FieldsWrapper";
 import OtherLocationsFields from "./OtherLocationsFields";
 import SearchIcon from '@mui/icons-material/Search';
 
-import { providerFormTypes } from '../../constants/provider_fields.js'
+import {providerFormTypes} from '../../constants/provider_fields.js'
 
 const useStyles = makeStyles((theme) => ({
-  link: {
-    color: 'inherit',
-    textDecorationLine: 'none',
-  },
-  linkWithHover: {
-    fontWeight: 500,
-    color: 'inherit',
-    // textDecorationLine: 'none',
-    '&:hover': {
-      textDecorationLine: 'underline',
-      color: '#2f5ac7'
+    link: {
+        color: 'inherit',
+        textDecorationLine: 'none',
+    },
+    linkWithHover: {
+        fontWeight: 500,
+        color: 'inherit',
+        // textDecorationLine: 'none',
+        '&:hover': {
+            textDecorationLine: 'underline',
+            color: '#2f5ac7'
+        }
+    },
+    progress: {
+        margin: theme.spacing(2),
+        marginTop: 50,
+    },
+    chipButton: {
+        paddingLeft: 6,
+        marginLeft: 6,
     }
-  },
-  progress: {
-    margin: theme.spacing(2),
-    marginTop: 50,
-  },
-  chipButton: {
-    paddingLeft: 6,
-    marginLeft: 6,
-  }
 }));
 
 export function Link({className = '', color, ...props}) {
-  const classes = useStyles();
-  return <DomLink {...props} className={color ? classes.linkWithHover : classes.link + ' ' + className}/>
+    const classes = useStyles();
+    return <DomLink {...props} className={color ? classes.linkWithHover : classes.link + ' ' + className}/>
 }
+
+const formFields = {
+    fieldName1: "Display Name 1",
+    fieldName2: "Display Name 2",
+    fieldName3: "Display Name 3",
+    fieldName4: "Display Name 4",
+    // Add more fields as needed
+};
+
+const renderFormFields = () => {
+    return Object.entries(formFields).map(([fieldName, displayName]) => (
+        <TextField
+            key={fieldName}
+            label={displayName}
+            variant="standard"
+            size="small"
+            style={{marginBottom: 10}}
+            // You can add more props or customize as needed
+        />
+    ));
+};
+
 
 /**
  * Used by DataTable. Add "add button" and "upload by CSV button"
@@ -61,123 +83,128 @@ export function Link({className = '', color, ...props}) {
  * @returns {*}
  * @constructor
  */
-export function CustomToolbar({handleAdd, handleUpload, handleSearch,  type}) {
-  const navigate = useNavigate();
-  const classes = useStyles();
+export function CustomToolbar({handleAdd, handleUpload, handleSearch, type}) {
+    const navigate = useNavigate();
+    const classes = useStyles();
 
-  const [anchorEl_provider, setAnchorEl_provider] = useState(null);
-  const [anchorEl_search, setAnchorEl_search] = useState(null);
-  const open_add_provider = Boolean(anchorEl_provider);
-  const open_search = Boolean(anchorEl_search);
+    const [anchorEl_provider, setAnchorEl_provider] = useState(null);
+    const [anchorEl_search, setAnchorEl_search] = useState(null);
+    const open_add_provider = Boolean(anchorEl_provider);
+    const open_search = Boolean(anchorEl_search);
 
-  const handleProviderAddClick = event => {
-      setAnchorEl_provider(event.currentTarget);
-  };
+    const handleProviderAddClick = event => {
+        setAnchorEl_provider(event.currentTarget);
+    };
 
-  const handleAdvanceSearchClick = event => {
-      setAnchorEl_search(event.currentTarget);
-  }
+    const handleAdvanceSearchClick = event => {
+        setAnchorEl_search(event.currentTarget);
+    }
 
-  const handleProviderClose = () => {
-      setAnchorEl_provider(null);
-  };
+    const handleProviderClose = () => {
+        setAnchorEl_provider(null);
+    };
 
-  const handleAdvanceSearchClose = () => {
-      setAnchorEl_search(null);
-  }
+    const handleAdvanceSearchClose = () => {
+        setAnchorEl_search(null);
+    }
 
-  const handleLink = link => () => {
-    navigate(link);
-  };
+    const handleLink = link => () => {
+        navigate(link);
+    };
 
-  const [searchItem, setSearchItem] = useState("");
+    const [searchItem, setSearchItem] = useState("");
 
-  const handleInputChange = (event) => {
-    setSearchItem(event.target.value);
-  };
+    const handleInputChange = (event) => {
+        setSearchItem(event.target.value);
+    };
 
 
+    return (
+        <>
+            <TextField
+                label="Search"
+                variant="outlined"
+                size="small"
+                // value={searchItem}
+                onChange={handleInputChange}
+            />
 
-  return (
-    <>
-      <TextField
-          label="Search"
-          variant="outlined"
-          size="small"
-          // value={searchItem}
-          onChange={handleInputChange}
-      />
+            <Button size="small" onClick={() => handleSearch(searchItem)}>Search</Button>
 
-      <Button size="small" onClick={() => handleSearch(searchItem)} >Search</Button>
+            {/*handleDelete*/}
+            {handleUpload && <Chip onClick={handleUpload}
+                                   color="default"
+                                   icon={<UploadIcon/>}
+                                   label="Upload"
+                                   variant="outlined"
+                                   className={classes.chipButton}
+            />}
 
-      {/*handleDelete*/}
-      {handleUpload && <Chip onClick={handleUpload}
-            color="default"
-            icon={<UploadIcon/>}
-            label="Upload"
-            variant="outlined"
-            className={classes.chipButton}
-      />}
+            <Chip onClick={handleAdvanceSearchClick}
+                  color="primary"
+                  icon={<SearchIcon/>}
+                  label="Advance Search"
+                  variant="outlined"
+                  className={classes.chipButton}
+            />
 
-      <Chip onClick={handleAdvanceSearchClick}
-            color="primary"
-            icon={<SearchIcon/>}
-            label="Advance Search"
-            variant="outlined"
-            className={classes.chipButton}
-      />
+            <Popper open={open_search} anchorEl={anchorEl_search} keepMounted={true}
+                    sx={{zIndex: 2}}>
+                <Container style={{display: 'flex', flexDirection: 'column'}}
+                           sx={{bgcolor: 'background.paper', border: 1, borderRadius: 5, marginTop: 1}}>
+                    {renderFormFields()}
+                    <Container >
+                        <Button onClick={handleAdvanceSearchClose}>Close</Button>
+                        <Button>Search</Button>
 
-      <Popper open={open_search} anchorEl={anchorEl_search} keepMounted={true} style={{zIndex:2}} >
-           <Box>
+                    </Container>
+                </Container>
+            </Popper>
 
-               <Button onClick={handleAdvanceSearchClose}>Close</Button>
+            <Chip onClick={type === 'providers' ? handleProviderAddClick : handleAdd}
+                  color="primary"
+                  icon={<AddIcon/>}
+                  label="Add"
+                  variant="outlined"
+                  className={classes.chipButton}
+            />
 
-           </Box>
-      </Popper>
-
-      <Chip onClick={type === 'providers' ? handleProviderAddClick : handleAdd}
-            color="primary"
-            icon={<AddIcon/>}
-            label="Add"
-            variant="outlined"
-            className={classes.chipButton}
-      />
-
-      {/*For Providers */}
-      {type === 'providers' &&
-      <Menu
-        id="long-menu"
-        anchorEl={anchorEl_provider}
-        keepMounted
-        open={open_add_provider}
-        onClose={handleProviderClose}
-      >
-        {Object.entries(providerFormTypes).map(([value, formType]) =>
-          <MenuItem disabled={formType !== 'Organization' && formType !== 'Volunteer'} key={formType} onClick={handleLink(`/providers/${value}/new`)}>
-            {formType}
-          </MenuItem>)
-        }
-      </Menu>
-      }
-    </>
-  );
+            {/*For Providers */}
+            {type === 'providers' &&
+                <Menu
+                    id="long-menu"
+                    anchorEl={anchorEl_provider}
+                    keepMounted
+                    open={open_add_provider}
+                    onClose={handleProviderClose}
+                >
+                    {Object.entries(providerFormTypes).map(([value, formType]) =>
+                        <MenuItem disabled={formType !== 'Organization' && formType !== 'Volunteer'} key={formType}
+                                  onClick={handleLink(`/providers/${value}/new`)}>
+                            {formType}
+                        </MenuItem>)
+                    }
+                </Menu>
+            }
+        </>
+    );
 }
 
 export function Loading({message = 'Loading Components...'}) {
-  const classes = useStyles();
-  return (
-    <div style={{textAlign: 'center'}}>
-      <CircularProgress className={classes.progress}/>
-      <Typography variant="subtitle2" color={"textSecondary"}>
-        {message}
-      </Typography>
-    </div>
-  );
+    const classes = useStyles();
+    return (
+        <div style={{textAlign: 'center'}}>
+            <CircularProgress className={classes.progress}/>
+            <Typography variant="subtitle2" color={"textSecondary"}>
+                {message}
+            </Typography>
+        </div>
+    );
 }
 
 
 export {
-  GoogleMap, CSVUploadModal, DeleteModal, DropdownMenu, GenericPage, FormStepper, FieldsWrapper,
-  OtherLocationsFields
+    GoogleMap, CSVUploadModal, DeleteModal, DropdownMenu, GenericPage, FormStepper, FieldsWrapper,
+    OtherLocationsFields
 }
 export {EnhancedTable as DataTable} from './Table';
