d9ba5208b06051ac889708a8f8b8c7aaec53dc4b
Subhasis Chakraborti <subhasis.chakraborti@mail.utoronto.ca>
2024-01-10 13:05:13 -0500

    Fix outcomeOccurrence link

--- frontend/src/components/outcomeOccurrence/ListClientOutcomeOccurrences.js
+++ frontend/src/components/outcomeOccurrence/ListClientOutcomeOccurrences.js
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
 import {Loading} from "../shared";
 import {getInstancesInClass} from "../../api/dynamicFormApi";
 import {fetchMultipleGeneric} from "../../api/genericDataApi";
@@ -7,6 +8,7 @@ import {Box, Fade, List, Button} from "@mui/material";
 export function ListClientOutcomeOccurrences({clientId, outcomeId}) {
   const [outcomeOccurrences, setOutcomeOccurrences] = useState([]);
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchMultipleGeneric('outcomeOccurrence')
@@ -26,7 +28,7 @@ export function ListClientOutcomeOccurrences({clientId, outcomeId}) {
     : <List sx={{ml: 4}}>
       {outcomeOccurrences.map((outcomeOccurrence, idx) => {
         return <Button key={idx} sx={{textTransform: 'none', mr: 1, mb: 1}}
-            variant="outlined">
+            variant="outlined" onClick={() => navigate(`/outcomeOccurrences/${outcomeOccurrence._id}`)}>
           Outcome Occurrence {outcomeOccurrence._id}
         </Button>;
       })}
