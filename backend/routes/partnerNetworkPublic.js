4911a4b46dcaa1669938ed35d5e0c3a3ac588de0
Subhasis Chakraborti <subhasis.chakraborti@mail.utoronto.ca>
2024-01-31 14:59:48 -0500

    API changes for sending and receiving appointments

--- backend/routes/partnerNetworkPublic.js
+++ backend/routes/partnerNetworkPublic.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const {sendOrganization} = require("../services/partnerNetwork");
 const {receiveReferral} = require("../services/partnerNetwork/referrals");
+const { receiveAppointment } = require('../services/partnerNetwork/appointments');
 const router = express.Router();
 router.get('/partnerNetwork/organization/', sendOrganization);
 
 router.post('/partnerNetwork/referral/', receiveReferral);
 router.put('/partnerNetwork/referral/', receiveReferral);
+
+router.post('/partnerNetwork/appointment/', receiveAppointment);
+router.put('/partnerNetwork/appointment/', receiveAppointment);
 
 module.exports = router;
