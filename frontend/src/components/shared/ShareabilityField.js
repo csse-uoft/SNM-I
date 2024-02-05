729e6db3301c44cc7bb714401892418db9c807a8
Subhasis Chakraborti <subhasis.chakraborti@mail.utoronto.ca>
2024-02-05 16:41:23 -0500

    Change ShareabilityField to use backend shareability options

--- frontend/src/components/shared/ShareabilityField.js
+++ frontend/src/components/shared/ShareabilityField.js
@@ -3,15 +3,17 @@ import {Loading} from "../shared";
 import {getPartnerOrganizations} from "../../api/partnerOrganization";
 import SelectField from "../shared/fields/SelectField";
 import MultiSelectField from "../shared/fields/MultiSelectField";
-import {Box, Fade} from "@mui/material";
+import {Fade} from "@mui/material";
+import {getInstancesInClass} from "../../api/dynamicFormApi";
 
 export function ShareabilityField({fields, handleChange, shareabilityFieldId, partnerOrganizationsFieldId}) {
   const shareabilityFieldKey = `characteristic_${shareabilityFieldId}`;
   const partnerOrganizationsFieldKey = `internalType_${partnerOrganizationsFieldId}`;
 
-  const [selectedShareability, setSelectedShareability] = useState(fields[shareabilityFieldKey] || "Not shareable");
+  const [selectedShareability, setSelectedShareability] = useState(fields[shareabilityFieldKey]);
   const [dynamicOptions, setDynamicOptions] = useState({});
-  const [loading, setLoading] = useState(true);
+  const [loadingPartners, setLoadingPartners] = useState(true);
+  const [loadingShareabilities, setLoadingShareabilities] = useState(true);
 
   const handleChangeShareability = key => (e) => {
     const value = e.target.value;
@@ -19,7 +21,7 @@ export function ShareabilityField({fields, handleChange, shareabilityFieldId, pa
     handleChange(key)(e);
 
     // if the new selectedShareability value is not "Shareable with all organizations", unset the inner field
-    if (value !== "Shareable with all organizations") {
+    if (dynamicOptions[":Shareability"][value] !== "Shareable with all organizations") {
       handleChange(partnerOrganizationsFieldKey)({target: {value: []}});
     }
   }
@@ -27,24 +29,24 @@ export function ShareabilityField({fields, handleChange, shareabilityFieldId, pa
   useEffect(() => {
     getPartnerOrganizations()
       .then(options => setDynamicOptions(prev => ({...prev, ":Organization": options})))
-      .then(() => setLoading(false));
+      .then(() => setLoadingPartners(false));
   }, []);
 
-  const options = {
-    "Shareable with partner organizations": 'Shareable with partner organizations',
-    "Shareable with all organizations": 'Shareable with all organizations',
-    "Not shareable": "Not shareable"
-  };
+  useEffect(() => {
+    getInstancesInClass(':Shareability')
+      .then(options => setDynamicOptions(prev => ({...prev, ":Shareability": options})))
+      .then(() => setLoadingShareabilities(false));
+  }, []);
 
-  if (loading) {
+  if (loadingPartners || loadingShareabilities) {
     return <Loading />
   }
 
   return <>
     <SelectField key={shareabilityFieldKey} label="Shareability" required value={fields[shareabilityFieldKey]}
-                 options={options} onChange={handleChangeShareability(shareabilityFieldKey)}/>
-    {selectedShareability === "Shareable with partner organizations" ?
-      <Fade in={selectedShareability === "Shareable with partner organizations"}>
+                 options={dynamicOptions[":Shareability"] || {}} onChange={handleChangeShareability(shareabilityFieldKey)}/>
+    {dynamicOptions[":Shareability"][selectedShareability] === "Shareable with partner organizations" ?
+      <Fade in={dynamicOptions[":Shareability"][selectedShareability] === "Shareable with partner organizations"}>
         <div>
           <MultiSelectField key={partnerOrganizationsFieldKey} label="Partner organizations to share with" required
                             value={fields[partnerOrganizationsFieldKey]} options={dynamicOptions[":Organization"] || {}}
