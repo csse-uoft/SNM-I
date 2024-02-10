bbaff73f5a91731e275d3eb33018cbe768744b8c
BenCheng2 <bencheng0609@gmail.com>
2024-02-10 17:59:16 -0500

    feat(Feature): Temp

--- frontend/src/components/calendar/PersonalCalendar.js
+++ frontend/src/components/calendar/PersonalCalendar.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Calendar from 'react-calendar';
 
 
 const PersonalCalendar = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+
   return (
       <div>
+
+        <Calendar onChange={handleDateChange} value={selectedDate} onClickDay={(value) => alert(value)}/>
+
       </div>
   );
 };
