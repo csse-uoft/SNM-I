b8a991096bbdfb5f5faadf9036a0fccfa6c02c61
BenCheng2 <bencheng0609@gmail.com>
2024-03-03 15:54:01 -0500

    feat(Feature): Big Calendar Frontend

--- frontend/src/components/calendar/PersonalCalendar.js
+++ frontend/src/components/calendar/PersonalCalendar.js
@@ -1,8 +1,14 @@
 import React, {useContext, useEffect, useState} from 'react';
-import Calendar from 'react-calendar';
+// import Calendar from 'react-calendar';
 import {fetchCalendarAppointments} from '../../api/calendarAPI';
 import {useParams} from "react-router-dom";
 import {UserContext} from "../../context";
+
+import { Calendar, momentLocalizer  } from 'react-big-calendar'
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from 'moment-timezone';
+import 'moment-timezone';
+import {FormControl, InputLabel, MenuItem, Select} from "@mui/material";
 
 function convertDate(date) {
   // Convert date to ISO string
@@ -11,19 +17,33 @@ function convertDate(date) {
 }
 
 const PersonalCalendar = () => {
-  const [selectedStartDate, setSelectedStartDate] = useState(new Date().toISOString());
-  const [selectedEndDate, setSelectedEndDate] = useState(new Date().toISOString());
+  // Requirements for the react-big-calendar
+  const localizer = momentLocalizer(moment);
 
+  // The variable to store the appointments that will be shown on the calendar
   const [appointments, setAppointments] = useState([]);
 
+  // Local storage might store teh timezone, if so, use that, otherwise use the default timezone
+  // This variable is used to initialize the timezone selector
+  const initialTimeZone = localStorage.getItem('timezone') || 'Atlantic/Faeroe';
+  const [selectedTimezone, setSelectedTimezone] = useState(initialTimeZone);
+
+
+  // Other variables for indexing
   const {id} = useParams();
 
 
   // Fetch appointments from the API
   const fetchAppointments = async () => {
-    const data = await fetchCalendarAppointments({user_id: id, startDate: selectedStartDate, endDate: selectedEndDate});
+    // Fetch the appointments from the backend
+    const data = await fetchCalendarAppointments({user_id: id});
+    // For each of the appointments, convert the start and end date to a Date object that can be used by the react-big-calendar
+    for (let i = 0; i < data.data.length; i++) {
+      data.data[i].start = moment.tz(data.data[i].start, selectedTimezone).toDate();
+      data.data[i].end = moment.tz(data.data[i].end, selectedTimezone).toDate();
+    }
+    // Set the appointments to the state
     setAppointments(data.data);
-    console.log(data.data)
   };
 
   // Fetch appointments when the component mounts
@@ -31,37 +51,56 @@ const PersonalCalendar = () => {
     fetchAppointments();
   }, []);
 
-  // Update the selected start and end date when a day is clicked
-  function UpdateClickedDay(day) {
-    day.setHours(0, 0, 0, 0)
-    setSelectedStartDate(convertDate(day));
-    day.setDate(day.getDate() + 1);
-    setSelectedEndDate(convertDate(day));
-
-    fetchAppointments();
+  // Handle the event click
+  const handleEventClick = (event) => {
+    // Get the event id
+    let event_id = event._id;
+    // Jump to the event page
+    window.location.href = `/appointments/${event_id}`;
   }
+
+  const handleTimeZoneChange = (event) => {
+    setSelectedTimezone(event.target.value); // Set the selected timezone state
+    moment.tz.setDefault(event.target.value); // Set the default timezone for moment
+    localStorage.setItem('timezone', event.target.value) // Save the selected timezone to local storage
+    // window.location.reload() // Reload the page to apply the timezone change
+
+    // Additional note: big calendar have a bug that the timezone change will lose the event display for the last column if not reloaded
+    // Therefore we need to reload the page to apply the timezone change
+  };
+
 
   return (
     <div>
+      <FormControl size={"small"} >
+        <InputLabel id="timezone-label">Time Zone</InputLabel>
+        <Select
+          labelId="timezone-label"
+          id="timezone-select"
+          value={selectedTimezone}
+          label="Time Zone"
+          onChange={handleTimeZoneChange}
+        >
+          {moment.tz.names().map((zone) => (
+            <MenuItem key={zone} value={zone}>
+              {zone}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
 
-      <Calendar onChange={(date) => {}} value={selectedStartDate}
-                onClickDay={(date) => UpdateClickedDay(date)}/>
+      <h2>Timezone: {selectedTimezone}</h2>
 
-      {selectedStartDate ? (<p>{selectedStartDate}</p>) : (<p>No date selected</p>)}
-      {selectedEndDate ? (<p>{selectedEndDate}</p>) : (<p>No date selected</p>)}
-
-      {appointments ? (
-        // Check if appointments is defined before mapping
-        appointments.map((appointment) => (
-          <p>{appointment._uri}</p>
-        ))
-      ) : (
-        // Render a loading message or handle the case when appointments is undefined
-        <p>Loading appointments...</p>
-      )}
+      <Calendar
+        localizer={localizer}
+        events = {appointments}
+        style={{height: 500, width: 1000}}
+        onSelectEvent={event => handleEventClick(event)}
+      />
 
     </div>
   );
 };
+
 
 export default PersonalCalendar;
