b3b8d622b764a07506297f84b4171970a69dfe85
BenCheng2 <bencheng0609@gmail.com>
2024-03-03 16:21:38 -0500

    feat(Feature): Update calendar query

--- backend/services/calendar/index.js
+++ backend/services/calendar/index.js
@@ -2,6 +2,8 @@ const {GDBAppointmentModel} = require("../../models/appointment");
 const {GDBServiceProviderModel} = require("../../models");
 const {extractAllIndexes} = require("../../helpers/stringProcess");
 const {graphdb} = require("../../config");
+const {GDBCharacteristicModel} = require("../../models/ClientFunctionalities/characteristic");
+const {GDBCOModel} = require("../../models/ClientFunctionalities/characteristicOccurrence");
 
 async function fetchAppointment(req, res, next) {
 
@@ -16,7 +18,29 @@ async function fetchAppointment(req, res, next) {
     let data_array = [];
     for (let i = 0; i < array.length; i++) {
       data_array.push(await GDBAppointmentModel.find({_uri: array[i]},
-        {populates: ['characteristicOccurrences.occurrenceOf', 'questionOccurrence', 'address']}));
+        {populates: ['characteristicOccurrences.occurrenceOf']}));
+
+      // Obtain the values from the characteristicOccurrences
+      // Iterate through the characteristicOccurrences
+      // for (let j = 0; j < data_array[i][0].characteristicOccurrences.length; j++) {
+      //   let occurrence = data_array[i][0].characteristicOccurrences[j];
+      //   if (occurrence.occurrenceOf) {
+      //     // Check if the occurrence is the start date of the appointment
+      //     if (occurrence.occurrenceOf.description === "Start date of a service Occurrence") {
+      //       // Get the date
+      //       data_array[i][0].set("start", occurrence.dataDateValue);
+      //       // data_array[i][0].set("end", new Date());
+      //     }
+      //     if (occurrence.occurrenceOf.description === "End date of a service Occurrence") {
+      //       // Get the date
+      //       data_array[i][0].set("end", occurrence.dataDateValue);
+      //     }
+      //     // Obtain the title of the appointment
+      //     if (occurrence.occurrenceOf.description === "Appointment Name") {
+      //       data_array[i][0].set("title", occurrence.dataStringValue);
+      //     }
+      //   }
+      // }
     }
     data = data_array.flat();
   }
@@ -28,12 +52,12 @@ async function fts_appointment_for_calendar(params) {
   // Assume all passed in date are in the format of "2024-02-22T00:00:00Z"
   const {user_id, startDate, endDate} = params;
 
-  let date_query;
-  if (!startDate || !endDate) {
-    date_query = ``;
-  } else {
-    date_query = `FILTER (?date >= xsd:dateTime("${startDate}") && ?date < xsd:dateTime("${endDate}")).`;
-  }
+  // let date_query;
+  // if (!startDate || !endDate) {
+  //   date_query = ``;
+  // } else {
+  //   date_query = `FILTER (?date >= xsd:dateTime("${startDate}") && ?date < xsd:dateTime("${endDate}")).`;
+  // }
 
   // The id of the user should be an integer like 1, 2, 3, etc.
   let person_query;
@@ -52,10 +76,7 @@ PREFIX  rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 SELECT DISTINCT  ?object1
 WHERE
   { ?object1  rdf:type  snmi:Appointment
-    { ?object1  snmi:hasCharacteristicOccurrence  ?date_object .
-      ?date_object  snmi:occurrenceOf  ?characteristicOccurrence_object ;
-                snmi:hasDateValue     ?date
-        ${date_query}
+    { 
         ${person_query}
     }
   }
