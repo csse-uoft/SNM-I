ca862ae58008ac5a5319900d7a243efc77858dee
Damon Ma <damondma@gmail.com>
2024-03-15 15:40:08 -0400

    Created Models for program waitlists + entries

---
+++ backend/models/program/programWaitlistEntry.js
@@ -0,0 +1,17 @@
+const {createGraphDBModel, DeleteType, Types} = require("graphdb-utils");
+const { GDBProgramRegistrationModel } = require("../serviceRegistration");
+
+const GDBProgramWaitlistEntryModel = createGraphDBModel({
+
+    programRegistration: {type: GDBProgramRegistrationModel, internalKey: ':hasProgramRegistration'},
+    priority: {type: Number, internalKey: ':hasPriority'},
+    date: {type: Date, internalKey: ':hasDate'},
+},
+{  
+    rdfTypes: [':ProgramWaitlistEntry'], name: 'programWaitlistEntry'
+}
+
+);
+module.exports = {
+    GDBProgramWaitlistEntryModel
+  }
