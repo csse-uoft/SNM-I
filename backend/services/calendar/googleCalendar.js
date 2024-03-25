6a38c2cebcf41ae011518ca329b88ada74fafea5
BenCheng2 <bencheng0609@gmail.com>
2024-03-25 15:28:48 -0400

    Google Calendar Progress: Backend Global Variables for Google Verification

--- backend/services/calendar/googleCalendar.js
+++ backend/services/calendar/googleCalendar.js
@@ -1,7 +1,8 @@
 const {google} = require('googleapis');
 
-const GOOGLE_CLIENT_ID = "97097151571-4mfu6di9l4qg6baibofrr3d3e46fh354.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = "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";
+require('dotenv').config();
+GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 const oAuth2Client = new google.auth.OAuth2(
   GOOGLE_CLIENT_ID,
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,8 +7,10 @@ import {GoogleOAuthProvider} from "@react-oauth/google";
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+const clientId = "97097151571-4mfu6di9l4qg6baibofrr3d3e46fh354.apps.googleusercontent.com";
+
 root.render(
-  <GoogleOAuthProvider clientId={"97097151571-4mfu6di9l4qg6baibofrr3d3e46fh354.apps.googleusercontent.com"}>
+  <GoogleOAuthProvider clientId={clientId}>
     <BrowserRouter>
       <App/>
     </BrowserRouter>
