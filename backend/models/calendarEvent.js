fcc5c00eb3180a9ec014662733d9ef92bdb7daaa
Murphy <murphytian06@gmail.com>
2024-11-11 21:47:15 +0800

    [Feat] CalendarEvent backend

---
+++ backend/models/calendarEvent.js
@@ -0,0 +1,25 @@
+const { createGraphDBModel, DeleteType, Types } = require("graphdb-utils");
+const { GDBClientModel } = require("./ClientFunctionalities/client");
+const { GDBUserAccountModel } = require("./userAccount");
+const {GDBPersonModel} = require("./person");
+const {GDBCOModel} = require("./ClientFunctionalities/characteristicOccurrence");
+const {GDBAddressModel} = require('./address');
+const { GDBReferralModel } = require("./referral");
+
+const GDBCalendarModel = createGraphDBModel ({
+  client: {type: GDBClientModel, internalKey: ':forClient'},
+  datetime: {type: Date, internalKey: ':hasDatetime'},
+  person: {type: GDBPersonModel, internalKey: ':hasPerson'},
+  user: {type: GDBUserAccountModel, internalKey: ':withUser'},
+  status: {type: String, internalKey: ':hasAppointmentStatus'},
+  referral: {type: GDBReferralModel, internalKey: ':hasReferral'},
+  characteristicOccurrences : {type: [GDBCOModel], internalKey: ':hasCharacteristicOccurrence'},
+  address: {type: GDBAddressModel, internalKey: 'ic:hasAddress', onDelete: DeleteType.CASCADE},
+  idInPartnerDeployment: {type: String, internalKey: ':hasIdInPartnerDeployment'},
+}, {
+  rdfTypes: [':CalendarEvent'], name: 'calendarEvent'
+});
+
+module.exports = {
+  GDBCalendarEventModel
+}
---
+++ backend/services/genericData/calendarEvent.js
@@ -0,0 +1,34 @@
+const {getPredefinedProperty} = require("./helperFunctions");
+const {GDBInternalTypeModel} = require("../../models/internalType");
+const {SPARQL} = require("graphdb-utils");
+const FORMTYPE = 'calendar-event';
+
+const calendarEventInternalTypeCreateTreater = async (internalType, instanceData, value) => { 
+  //get the property name from the internalType
+  const property = getPredefinedProperty(FORMTYPE, internalType);
+  //if the property is client, person or user, then set the value to the instanceData
+  if (property === 'client' || property === 'person' || property === 'user' || property === 'referral') {
+    instanceData[property] = value;
+  }
+};
+
+const calendarEventInternalTypeFetchTreater = async (data) => {
+  const result = {};
+  const schema =  data.schema; 
+  // for each property in data, if the property is client, person or user, then set the value to the result
+  for (const property in data) {
+    if (property === 'client' || property === 'person' || property === 'user' || property === 'referral') {
+      const internalType = await GDBInternalTypeModel.findOne({predefinedProperty: schema[property].internalKey, formType: FORMTYPE});
+      result[ 'internalType_'+ internalType._id] = SPARQL.ensureFullURI(data[property]);
+    }
+  }
+  return result;
+};
+
+
+const calendarEventInternalTypeUpdateTreater = async (internalType, value, result) => {
+  await appointmentInternalTypeCreateTreater(internalType, result, value);
+}
+
+
+module.exports = {calendarEventInternalTypeCreateTreater, calendarEventInternalTypeFetchTreater, calendarEventInternalTypeUpdateTreater}
